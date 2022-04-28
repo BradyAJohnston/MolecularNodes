402a3ffb07f01a3922bf7b12a1e81ef4ecb82950
Brady Johnston <brady.johnston@me.com>
2022-04-28 17:47:26 +0800

    possible fix for failed atomium import

--- src/install_packages.py
+++ src/install_packages.py
@@ -1,34 +1,29 @@
-#For Blender 2.92:
-#You have to have admininistor priviledges so for Windows users
-#JUST THIS ONCE open Blender by R-clicking on it in the start menu and select"Run as Administrator"
-
-#Open up the System Console (under the Window menu)
-#Then paste and run the following into the scripting workspace and run it
-# After it has run, then close and reopen Blender normally
-# You can then then use the module as normal with 
-# import MODULE
-
-#--- FROM HERE ---#
-
 import subprocess
 import sys
 import os
+import site
  
 # path to python.exe
 python_exe = os.path.realpath(sys.executable)
  
 # upgrade pip
 subprocess.call([python_exe, "-m", "ensurepip"])
-subprocess.call([python_exe, "-m", "pip", "install", "--upgrade", "pip"])
+subprocess.call([python_exe, "-m", "pip", "install", "--upgrade", "pip"], timeout=600)
  
 # install required packages
-subprocess.call([python_exe, "-m", "pip", "install", "atomium"])
+subprocess.call([python_exe, "-m", "pip", "install", "atomium"], timeout=600)
 
-print("Atomium Successfully Installed")
+def verify_user_sitepackages():
+    usersitepackagespath = site.getusersitepackages()
 
+    if os.path.exists(usersitepackagespath) and usersitepackagespath not in sys.path:
+        sys.path.append(usersitepackagespath)
 
+verify_user_sitepackages()
 
-#def install(package):
-#    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-#install("atomium")
+try:
+    import atomium
+    atomium.fetch("1bna")
+    atomium_install_successful = True
+except:
+    atomium_install_successful = False
