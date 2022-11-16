785c6b2e29a597f0acb001d447ab60a965627f4c
Brady <brady.johnston@me.com>
2022-11-16 11:35:12 +0800

    added notes and some cleanup

---
+++ notes.md
@@ -0,0 +1,2 @@
+- pdbx format doesn't have include_bonds as an option (but can be computed afterwards)
+
--- src/open.py
+++ src/open.py
@@ -1,57 +1,52 @@
+import bpy
+import numpy as np
+import biotite.structure as struc
+import biotite.structure.io.pdb as pdb
+import biotite.structure.io.pdbx as pdbx
+import biotite.structure.io.mmtf as mmtf
+import biotite.database.rcsb as rcsb
 
 def open_structure_rcsb(pdb_code, include_bonds = True):
-    import biotite.structure as struc
-    import biotite.database.rcsb as rcsb
-    import biotite.structure.io.mmtf as mmtf
-
     file = mmtf.MMTFFile.read(rcsb.fetch(pdb_code, "mmtf"))
     mol = mmtf.get_structure(file, model = 1, extra_fields = ["b_factor", "charge"], include_bonds = include_bonds)
     return mol
 
 
 def open_structure_local_pdb(file_path, include_bonds = True):
-    import biotite.structure as struc
-    import biotite.structure.io.pdb as pdb
-
     file = pdb.PDBFile.read(file_path)
     mol = pdb.get_structure(file, model = 1, extra_fields = ['b_factor', 'charge'], include_bonds = include_bonds)
-
     return mol
 
 def open_structure_local_pdbx(file_path, include_bonds = True):
-    import biotite.structure as struc
-    import biotite.structure.io.pdbx as pdbx
-
     file = pdbx.PDBxFile.read(file_path)
     mol  = pdbx.get_structure(file, model = 1, extra_fields = ['b_factor', 'charge'])
-
+    # for some reason .pdbx doesn't have an option for bonds on import, so manually create
+    # them here if requested
     if include_bonds:
         mol.bonds = struc.bonds.connect_via_residue_names(mol, inter_residue = True)
-
     return mol
 
 def mn_collection():
-    
     coll = bpy.data.collections.get('MolecularNodes')
     if not coll:
         coll = bpy.data.collections.new('MolecularNodes')
         bpy.context.scene.collection.children.link(coll)
     return coll
 
-def create_model(name, collection, locations, bonds=[], faces=[]):
+def create_model(name, collection, locations, bonds=[]):
     """
     Creates a mesh with the given name in the given collection, from the supplied
     values for the locationso of vertices, and if supplied, bonds and faces.
     """
     # create a new mesh
-    atom_mesh = bpy.data.meshes.new(name)
-    atom_mesh.from_pydata(locations, bonds, faces)
-    new_object = bpy.data.objects.new(name, atom_mesh)
+    mol_mesh = bpy.data.meshes.new(name)
+    mol_mesh.from_pydata(locations, bonds, faces=[])
+    mol_object = bpy.data.objects.new(name, mol_mesh)
     collection.objects.link(new_object)
-    return new_object
+    return mol_object
 
-def add_attribute(object, name, data, type = "FLOAT", domain = "POINT", add_attribute = True):
-    if not add_attribute:
+def add_attribute(object, name, data, type = "FLOAT", domain = "POINT", add = True):
+    if not add:
         return
     attribute = object.data.attributes.new(name, type, domain)
     attribute.data.foreach_set('value', data)
@@ -68,7 +63,7 @@ def MOL_import_mol(mol, mol_name, center_molecule = False, del_solvent = False, 
     packages.install_packages()
     packages.verify()
 
-    # remove the waters from the structure (most people don't want them anyway)
+    # remove the solvent from the structure if requested
     if del_solvent:
         mol = mol[np.invert(struc.filter_solvent(mol))]
 
@@ -82,9 +77,9 @@ def MOL_import_mol(mol, mol_name, center_molecule = False, del_solvent = False, 
 
     if include_bonds:
         bonds = mol.bonds.as_array()
-        mod = create_model(name = mol_name, collection = mn_collection(), locations = locations, bonds = bonds[:, [0,1]])
+        mol_object = create_model(name = mol_name, collection = mn_collection(), locations = locations, bonds = bonds[:, [0,1]])
     else:
-        mod = create_model(name = mol_name, collection = mn_collection(), locations = locations)
+        mol_object = create_model(name = mol_name, collection = mn_collection(), locations = locations)
 
     # compute some of the attributes before assignment
     atomic_number = np.fromiter(map(lambda x: dict.elements.get(x, {'atomic_number': 0}).get("atomic_number"), np.char.title(mol.element)), dtype = np.int)
@@ -96,16 +91,16 @@ def MOL_import_mol(mol, mol_name, center_molecule = False, del_solvent = False, 
 
     if include_bonds:
         bond_type = bonds[:, 2].copy(order = 'C') # the .copy(order = 'C') is to fix a weird ordering issue with the resulting array
-        add_attribute(mod, 'bond_type', bond_type,"INT", "EDGE")
+        add_attribute(mol_object, 'bond_type', bond_type,"INT", "EDGE")
 
     # assign the attributes to the object
-    add_attribute(mod, 'res_id', mol.res_id, "INT")
-    add_attribute(mod, 'res_name', res_name, "INT")
-    add_attribute(mod, "atomic_number", atomic_number, "INT", "POINT")
-    add_attribute(mod, "b_factor", mol.b_factor, "FLOAT", "POINT")
-    add_attribute(mod, "is_backbone", struc.filter_backbone(mol),"BOOLEAN", "POINT")
-    add_attribute(mod, "is_alpha_carbon", is_alpha, "BOOLEAN", "POINT")
-    add_attribute(mod, 'is_hetero', mol.hetero, "BOOLEAN")
-    add_attribute(mod, "vdw_radii", vdw_radii, "FLOAT", "POINT")
-    add_attribute(mod, "chain_id", chain_id, "INT", "POINT")
-    add_attribute(mod, 'is_solvent', is_solvent, "BOOLEAN", "POINT")
+    add_attribute(mol_object, 'res_id', mol.res_id, "INT")
+    add_attribute(mol_object, 'res_name', res_name, "INT")
+    add_attribute(mol_object, "atomic_number", atomic_number, "INT", "POINT")
+    add_attribute(mol_object, "b_factor", mol.b_factor, "FLOAT", "POINT")
+    add_attribute(mol_object, "is_backbone", struc.filter_backbone(mol),"BOOLEAN", "POINT")
+    add_attribute(mol_object, "is_alpha_carbon", is_alpha, "BOOLEAN", "POINT")
+    add_attribute(mol_object, 'is_hetero', mol.hetero, "BOOLEAN")
+    add_attribute(mol_object, "vdw_radii", vdw_radii, "FLOAT", "POINT")
+    add_attribute(mol_object, "chain_id", chain_id, "INT", "POINT")
+    add_attribute(mol_object, 'is_solvent', is_solvent, "BOOLEAN", "POINT")
