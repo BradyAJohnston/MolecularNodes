1a352b91641d83b70ff58b21c4106f95da623872
Brady Johnston <36021261+BradyAJohnston@users.noreply.github.com>
2022-11-21 17:34:29 +0800

    update name

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@
 
 
 bl_info = {
-    "name"        : "Molecular Nodes2 boogaloo",
+    "name"        : "Molecular Nodes 2: Electric Boogaloo",
     "author"      : "Brady Johnston", 
     "description" : "Some more nodes",
     "blender"     : (3, 3, 0),
