22d8c6e2035fa7ebd5e67e1cbc795213d1d676cf
Brady Johnston <brady.johnston@me.com>
2023-04-29 23:13:58 +0800

    playing with more potential tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,17 +9,17 @@ def bpy_module(cache):
     return cache.get("bpy_module", None)
 
 # ensure we can successfully install all of the required pacakges 
-def test_install_packages(bpy_module):
-    mn.pkg.install_all_packages()
-    assert 1 == 1
+# def test_install_packages(bpy_module):
+#     mn.pkg.install_all_packages()
+#     assert 1 == 1
 
 def test_versionID_pass(bpy_module):
-    expect_version = (2, 5, 2)
+    expect_version = (2, 6, 2)
     return_version = get_version(bpy_module)
     assert expect_version == return_version
 
 def test_versionID_fail(bpy_module):
-    expect_version = (0, 1, 1)
+    expect_version = (2, 5, 0)
     return_version = get_version(bpy_module)
     assert not expect_version == return_version
 
@@ -27,3 +27,9 @@ def test_versionID_fail(bpy_module):
 def test_load_rcsb(bpy_module):
     mol, file = mn.load.open_structure_rcsb('4ozs')
     assert 1 == 1
+
+def test_node_surface(bpy_module):
+    obj = mn.load.molecule_rcsb('6n2y')
+    name = 'MOL_style_surface_split_6n2y'
+    split_surface_node = mn.nodes.create_custom_surface(name, len(obj['chain_id_unique']))
+    assert split_surface_node.name == name
