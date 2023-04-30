af9eaca2161d1d5c6ed34db0b2f634aa2abeb39f
Brady Johnston <brady.johnston@me.com>
2023-04-30 14:05:39 +0800

    some more working tests

---
+++ tests/test_nodes.py
@@ -0,0 +1,9 @@
+import MolecularNodes as mn
+import bpy
+import pytest
+
+def test_node_surface(bpy_module):
+    obj = mn.load.molecule_rcsb('6n2y')
+    name = 'MOL_style_surface_split_6n2y'
+    split_surface_node = mn.nodes.create_custom_surface(name, len(obj['chain_id_unique']))
+    assert split_surface_node.name == name
