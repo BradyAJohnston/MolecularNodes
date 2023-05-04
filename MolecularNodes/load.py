ae29fba7e1f67c0097b207b0ed68bc1fa73c956e
wjm41 <wjm41@cam.ac.uk>
2023-05-04 16:49:09 +0100

    raise ValueError if ESMFold returns an error

--- MolecularNodes/load.py
+++ MolecularNodes/load.py
@@ -165,18 +165,22 @@ def open_structure_esm_fold(amino_acid_sequence, include_bonds=True):
 
     r = requests.post('https://api.esmatlas.com/foldSequence/v1/pdb/', data=amino_acid_sequence)
     
-    esm_folded_pdb_str = r.text
-    with io.StringIO() as f:
-        f.write(esm_folded_pdb_str)
-        f.seek(0)
-        file = pdb.PDBFile.read(f)
+    if r.ok:
     
-    # returns a numpy array stack, where each array in the stack is a model in the 
-    # the file. The stack will be of length = 1 if there is only one model in the file
-    mol = pdb.get_structure(file, extra_fields = ['b_factor', 'charge'], include_bonds = include_bonds)
-    return mol, file
+        esm_folded_pdb_str = r.text
+        with io.StringIO() as f:
+            f.write(esm_folded_pdb_str)
+            f.seek(0)
+            file = pdb.PDBFile.read(f)
+    
+        # returns a numpy array stack, where each array in the stack is a model in the 
+        # the file. The stack will be of length = 1 if there is only one model in the file
+        mol = pdb.get_structure(file, extra_fields = ['b_factor', 'charge'], include_bonds = include_bonds)
+        return mol, file
 
-
+    else:
+        raise ValueError(f'ESMFold returned an error for the amino acid sequence input. This is the error message: {r.text}')
+    
 def open_structure_local_pdb(file_path, include_bonds = True):
     import biotite.structure.io.pdb as pdb
     
