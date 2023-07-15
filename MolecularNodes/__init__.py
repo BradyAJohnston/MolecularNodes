88d317f68bd9a166b2c20d59e66d44c8a0097f7b
Brady Johnston <brady.johnston@me.com>
2023-07-15 09:37:45 +0800

    bump versions

--- MolecularNodes/__init__.py
+++ MolecularNodes/__init__.py
@@ -17,7 +17,7 @@ bl_info = {
     "author"      : "Brady Johnston", 
     "description" : "Toolbox for molecular animations in Blender & Geometry Nodes.",
     "blender"     : (3, 5, 0),
-    "version"     : (2, 7, 4),
+    "version"     : (2, 7, 5),
     "location"    : "Scene Properties -> MolecularNodes",
     "warning"     : "",
     "doc_url"     : "https://bradyajohnston.github.io/MolecularNodes/", 
