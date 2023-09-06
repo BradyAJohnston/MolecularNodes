65eec8c262c166ff2997558246734f4ec7be0f15
Brady Johnston <brady.johnston@me.com>
2023-09-06 08:14:13 +0800

    auto generate nodes_auto.qmd

--- docs/docs.py
+++ docs/docs.py
@@ -15,35 +15,52 @@ for node in bpy.data.node_groups:
     if node.name.startswith("MN"):
         nodes.append(node)
 
-node_1 = nodes[2]
 objects = []
 for node in nodes:
     input_list = []
-    # input_list.append(
-    #     griffe.docstrings.dataclasses.DocstringSectionText(title = None, value = node.name)
-    # )
     for input in node.inputs:
-        if input.type in ["VALUE", "INT", "BOOLEAN"]:
-            if input.type != "BOOLEAN":
-                value = round(input.default_value, 2)
+        if input.type in ["VALUE", "INT", "BOOLEAN", 'VECTOR']:
+            
+            if input.type == "VALUE":
+                default = round(input.default_value, 2)
+            if input.type == "VECTOR":
+                default = list(input.default_value)
             else:
-                value = input.default_value
+                default = input.default_value
             input_list.append(
                 griffe.docstrings.dataclasses.DocstringParameter(
                     name = input.name, 
                     annotation = input.type, 
-                    value = value, 
+                    value = default, 
                     description = input.description
                 )
             )
-    text = griffe.docstrings.dataclasses.DocstringSectionText(title = None, value = node.name)
+    title = node.name.replace('MN_', '').replace('_', ' ').title()
+    title = griffe.docstrings.dataclasses.DocstringSectionText(title = None, value = f"### {title}")
+    video = griffe.docstrings.dataclasses.DocstringSectionText(title = None, value = f"![](videos/{node.name}.mp4)")
     inputs = griffe.docstrings.dataclasses.DocstringSectionParameters(input_list)
-    objects.append([text, inputs])
+    objects.append([title, video, inputs])
 
 ren = MdRenderer(header_level = 2)
 
-for doc in objects:
-    for sec in doc:
-        print(ren.render(sec))
-        print("\n")
-    print("\n")
+with open("docs/nodes/nodes_auto.qmd", "w") as file:
+    file.write(
+        """
+---
+title: Node Documentation
+toc: true
+toc-depth: 3
+---
+"""
+        )
+    # new_section = True
+    # sections = ['animation', 'style', 'dna', 'select', 'utils']
+    for doc in objects:
+        section = ''
+        for sec in doc:
+            text = ren.render(sec)
+            
+            file.write(text)
+            file.write("\n")
+            file.write("\n")
+        file.write("\n")
