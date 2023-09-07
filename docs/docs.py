f65908629bf407e1480e594b74fb56e0fa3697ab
Brady <brady.johnston@me.com>
2023-09-07 10:45:15 +0800

    added colors

--- docs/docs.py
+++ docs/docs.py
@@ -4,51 +4,79 @@ import griffe
 import os
 import pathlib
 import json
+import colorsys
 
-with open('docs/nodes.json', 'r') as file:
-    node_descriptions = json.load(file)
 
 folder = pathlib.Path(__file__).resolve().parent
-mn_data_file = os.path.join(folder, "../MolecularNodes/assets/MN_data_file.blend")
 
+nodes_json = os.path.join(folder, "nodes.json")
+output_file = os.path.join(folder, "nodes.qmd")
+
+# load the long-form descriptions of the nodes from the nodes.json file
+with open(nodes_json, 'r') as file:
+    node_descriptions = json.load(file)
+
+# load the data file which contains the nodes
+mn_data_file = os.path.join(folder, "../MolecularNodes/assets/MN_data_file.blend")
 bpy.ops.wm.open_mainfile(filepath = mn_data_file)
+
+def rgba2hex(rgba):
+    r = int(rgba[0] * 255)
+    g = int(rgba[1] * 255)
+    b = int(rgba[2] * 255)
+    a = int(rgba[3] * 255)
+    return f'#{a:02x}{r:02x}{g:02x}{b:02x}'
+
+def rgb_01_to_hex(r, g, b):
+    r, g, b = [int(x * 255) for x in colorsys.rgb_to_hsv(r, g, b)]
+    return f'#{r:02x}{g:02x}{b:02x}'
 
 nodes = []
 
+# get the relevant nodes to write documentation about. Currentling limiting it to 
+# those that start with "MN". The nodes which start with ".MN" are for interal use only.
 for node in bpy.data.node_groups:
     if node.name.startswith("MN"):
         nodes.append(node)
 
-def get_values(inputs_outputs):
-    value_list = []
-    for input in inputs_outputs:
-        if input.type in ["VALUE", "INT", "BOOLEAN", 'VECTOR', 'MATERIAL', 'GEOMETRY']:
-            
-            if input.type == "VALUE":
-                default = round(input.default_value, 2)
-            elif input.type == "GEOMETRY":
-                default = None
-            elif input.type == "VECTOR":
-                default = list(input.default_value)
-            elif input.type == "MATERIAL":
-                default = 'MN_atomic_style'
-            else:
-                default = input.default_value
-            
-            type = input.type
-            subtype = input.bl_subtype_label
-            if subtype != 'None':
-                type = f"{type} ({subtype})"
-            
-            value_list.append(
-                griffe.docstrings.dataclasses.DocstringParameter(
-                    name = input.name, 
-                    annotation = type, 
-                    value = default, 
-                    description = input.description
-                )
+accepted_types = ["Float", "Integer", "Boolean", 'Vector', 'Material', 'Geometry', 'Color']
+
+def get_values(sockets):
+    parameter_list = []
+    for socket in sockets:
+        type = socket.bl_label
+        subtype = socket.bl_subtype_label
+        
+        if type not in accepted_types:
+            continue
+        
+        if type == "Float":
+            default = round(socket.default_value, 2)
+        elif type == "Geometry":
+            default = None
+        elif type == "Vector":
+            default = [round(value, 2) for value in socket.default_value]
+        elif type == "Material":
+            default = 'MN_atomic_style'
+        elif type == 'Color':
+            values = [int(val * 255) for val in list(socket.default_value)[0:3]]
+            # color = rgba2hex(list(socket.default_value))
+            default = "rgb({}, {}, {})".format(*values)
+        else:
+            default = socket.default_value
+        
+        if subtype != 'None':
+            type = f"{type} ({subtype})"
+        
+        parameter_list.append(
+            griffe.docstrings.dataclasses.DocstringParameter(
+                name = socket.name, 
+                annotation = type, 
+                value = default, 
+                description = socket.description
             )
-    return value_list
+        )
+    return parameter_list
 
 objects = []
 cat = ''
@@ -93,7 +121,7 @@ toc-depth: 3
 ---
 """
 
-with open("docs/nodes.qmd", "w") as file:
+with open(output_file, "w") as file:
     file.write(header)
     for doc in objects:
         section = ''
