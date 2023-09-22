1e0c0f4ea1a431b2e9d3bc1c9323be178dd6017f
Yuxuan Zhuang <yuxuan.zhuang@dbb.su.se>
2023-09-22 18:23:08 +0200

    add test for in_memory

--- tests/test_mda.py
+++ tests/test_mda.py
@@ -10,13 +10,13 @@ from .utils import get_verts, apply_mods, remove_all_molecule_objects
 
 @pytest.mark.skipif(not HAS_mda, reason='MDAnalysis is not installed')
 class TestMDA:
-    @pytest.fixture(scope='module')
+    @pytest.fixture(scope='module', autouse=True)
     def mda_session(self):
         mda_session = mn.mda.MDAnalysisSession()
         return mda_session
 
 
-    @pytest.fixture(scope='module')
+    @pytest.fixture(scope='module', autouse=True)
     def universe(self):
         top = "tests/data/md_ppr/box.gro"
         traj = "tests/data/md_ppr/first_5_frames.xtc"
@@ -24,38 +24,39 @@ class TestMDA:
         return u
 
 
-    def test_persistent_handlers_added(self, mda_session):
+    def test_persistent_handlers_added(self):
         assert bpy.app.handlers.load_post[-1].__name__ == '_rejuvenate_universe'
         assert bpy.app.handlers.save_pre[-1].__name__ == '_sync_universe'
 
 
-    def test_create_mda_session(self, mda_session):
+    def test_create_mda_session(self):
         assert mda_session is not None
         assert mda_session.uuid is not None
         assert mda_session.world_scale == 0.01
 
 
-    def reload_mda_session(self, mda_session):
+    def reload_mda_session(self):
         with pytest.warns(UserWarning, match='The existing mda session'):
             mda_session_2 = mn.mda.create_session() 
         assert mda_session.uuid == mda_session_2.uuid
 
-
-    def test_show_universe(self, snapshot, mda_session, universe):
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_show_universe(self, snapshot, in_memory):
         remove_all_molecule_objects(mda_session)
-        mda_session.show(universe)
+        mda_session.show(universe, in_memory=in_memory)
         obj = bpy.data.objects['atoms']
         verts = get_verts(obj, apply_modifiers = False)
         
         snapshot.assert_match(verts, 'md_gro_xtc_verts.txt')
 
-
-    def test_same_name_atoms(self, snapshot, mda_session, universe):
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_same_name_atoms(self, snapshot, in_memory):
         remove_all_molecule_objects(mda_session)
-        mda_session.show(universe)
+        mda_session.show(universe, in_memory=in_memory)
 
         with pytest.warns(UserWarning, match='The name of the object is changed'):
-            mda_session.show(universe)
+            mda_session.show(universe, in_memory=in_memory)
+
         obj_1 = bpy.data.objects['atoms']
         obj_2 = bpy.data.objects['atoms.001']
         verts_1 = get_verts(obj_1, apply_modifiers = False)
@@ -63,11 +64,12 @@ class TestMDA:
         
         assert(verts_1 == verts_2)
 
-
-    def test_show_multiple_selection(self, snapshot, mda_session, universe):
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_show_multiple_selection(self, snapshot, in_memory):
         remove_all_molecule_objects(mda_session)
         custom_selections = {'name_ca': 'name CA'}
         mda_session.show(universe,
+                        in_memory=in_memory,
                         name='protein',
                         selection='protein',
                         custom_selections=custom_selections)
@@ -75,14 +77,45 @@ class TestMDA:
         verts = get_verts(obj, apply_modifiers = False)
 
         snapshot.assert_match(verts, 'md_gro_xtc_verts_protein.txt')
-        obj_ca = bpy.data.objects['name_ca']
-        verts_ca = get_verts(obj_ca, apply_modifiers = False)
-        snapshot.assert_match(verts_ca, 'md_gro_xtc_verts_ca.txt')
-
-
-    def test_trajectory_update(self, snapshot, mda_session, universe):
+        
+        # different bahavior in_memory or not.
+        if not in_memory:
+            obj_ca = bpy.data.objects['name_ca']
+            verts_ca = get_verts(obj_ca, apply_modifiers = False)
+            snapshot.assert_match(verts_ca, 'md_gro_xtc_verts_ca.txt')
+        else:
+            # attributes is added as name_ca.
+            TODO
+    
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_include_bonds(self, in_memory):
         remove_all_molecule_objects(mda_session)
-        mda_session.show(universe)
+        mda_session.show(universe,
+                         in_memory=in_memory,
+                         include_bonds=False)
+        obj = bpy.data.objects['atoms']
+        assert obj.edges == []
+
+        remove_all_molecule_objects(mda_session)
+        mda_session.show(universe, include_bonds=True)
+        obj = bpy.data.objects['atoms']
+        assert obj.edges != []
+    
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_attributes_added(self, in_memory):
+        remove_all_molecule_objects(mda_session)
+        mda_session.show(universe,
+                         in_memory=in_memory
+                         include_bonds=False)
+        obj = bpy.data.objects['atoms']
+        attributes = obj.attributes
+        snapshot.assert_match(attributes, 'md_gro_xtc_attributes.txt')
+
+
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_trajectory_update(self, snapshot, in_memory):
+        remove_all_molecule_objects(mda_session)
+        mda_session.show(universe, in_memory=in_memory)
         obj = bpy.data.objects['atoms']
 
         verts_frame_0 = get_verts(obj, apply_modifiers = False)
@@ -97,10 +130,11 @@ class TestMDA:
         assert(verts_frame_0 != verts_frame_1)
 
 
-    def test_show_updated_atoms(self, snapshot, mda_session, universe):
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_show_updated_atoms(self, snapshot, in_memory):
         remove_all_molecule_objects(mda_session)
         updating_ag = universe.select_atoms('around 5 resid 1', updating=True)
-        mda_session.show(updating_ag)
+        mda_session.show(updating_ag, in_memory=in_memory)
 
         obj = bpy.data.objects['atoms']
 
@@ -117,12 +151,26 @@ class TestMDA:
         assert(verts_frame_0 != verts_frame_1)
 
 
-    def test_save_persistance(self, snapshot, mda_session, universe, tmp_path):
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_update_deleted_objects(self, snapshot):
         remove_all_molecule_objects(mda_session)
-        mda_session.show(universe)
+        mda_session.show(universe, in_memory=in_memory)
+        bpy.data.objects.remove(bpy.data.objects['atoms'])
+
+        assert mda_session.universe_reps == {}
+        assert mda_session.atom_reps == {}
+        assert mda_session.rep_names == []
+
+
+    @pytest.mark.parametrize('in_memory', [False, True])
+    def test_save_persistance(self, snapshot, tmp_path, in_memory):
+        remove_all_molecule_objects(mda_session)
+        mda_session.show(universe, in_memory=in_memory)
         # save
         bpy.ops.wm.save_as_mainfile(filepath=str(tmp_path / 'test.blend'))
 
+        assert os.path.exists(f'{mda_session.session_tmp_dir}/{mda_session.uuid}.pkl')
+        
         # reload
         remove_all_molecule_objects(mda_session)
         bpy.ops.wm.open_mainfile(filepath=str(tmp_path / 'test.blend'))
