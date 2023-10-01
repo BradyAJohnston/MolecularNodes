eb2d61c9668ab4ce87e5a7beb3bbf1c9d34972dc
Brady Johnston <brady.johnston@me.com>
2023-10-01 13:48:40 +0800

    add presets to UI

--- docs/nodes.py
+++ docs/nodes.py
@@ -76,12 +76,10 @@ for node in nodes:
         continue
     entry_list = []
     name = node.name
-    info = node_descriptions.get(name)
-    try:
-        desc = info.get('description')
-        url  = info.get('video_url')
-    except AttributeError:
-        pass
+    info = node_descriptions.get(name, {'desc': None, 'url': None})
+    
+    desc = info.get('description')
+    url  = info.get('video_url')
     
     inputs = params(get_values(node.inputs))
     outputs = params(get_values(node.outputs))
@@ -95,7 +93,8 @@ for node in nodes:
     
     if desc:
         entry_list.append(text(title = None, value = desc))
-    entry_list.append(text(title = None, value = f"![]({url}.mp4)"))
+    if url:
+        entry_list.append(text(title = None, value = f"![]({url}.mp4)"))
 
     
     if len(inputs.as_dict()['value']) > 0:
