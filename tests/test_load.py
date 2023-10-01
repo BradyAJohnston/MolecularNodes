238a1d477dd6b1c0daadde81b36c73317efcd331
Brady Johnston <brady.johnston@me.com>
2023-10-01 22:51:56 +0800

    initial support for building assembly on import

--- tests/test_load.py
+++ tests/test_load.py
@@ -6,13 +6,14 @@ import numpy as np
 from .utils import get_verts, apply_mods, insert_last_node, realize_intances
 from . import utils as u
 
-codes = ['6n2y', '4ozs', '8H1B', '1BNA']
+codes = ['6n2y', '4ozs', '8H1B', '1BNA', '8EM6']
 styles = mn.nodes.styles_mapping.keys()
 
 @pytest.mark.parametrize("style", styles)
 @pytest.mark.parametrize("code", codes)
-def test_style(snapshot, style, code):
-    obj = mn.load.molecule_rcsb(code, starting_style=style)
+@pytest.mark.parametrize("assembly", [True, False])
+def test_style(snapshot, style, code, assembly):
+    obj = mn.load.molecule_rcsb(code, starting_style=style, build_assembly=True)
     last, output = u.get_nodes_last_output(obj.modifiers['MolecularNodes'].node_group)
     for input in last.inputs:
         if input.name == "Atom: Eevee / Cycles":
