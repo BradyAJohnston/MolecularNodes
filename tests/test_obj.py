78f123a8cfa297e96bbf5709b3e61ce2dabbb5e5
Brady Johnston <brady.johnston@me.com>
2023-11-02 17:20:59 +0800

    tests for set position

--- tests/test_obj.py
+++ tests/test_obj.py
@@ -1,6 +1,7 @@
 import bpy
+import numpy as np
 import molecularnodes as mn
-from .utils import apply_mods, get_verts
+from .utils import apply_mods, get_verts, sample_attribute
 
 def test_creat_obj():
     # Create a mesh object named "MyMesh" in the collection "MyCollection"
@@ -13,3 +14,15 @@ def test_creat_obj():
     assert len(my_object.data.vertices) == 3
     assert my_object.name == name
     assert my_object.name != "name"
+
+def test_set_position():
+    mol = mn.load.molecule_rcsb('4ozs')
+    
+    pos_a = sample_attribute(mol, 'position')
+    
+    mn.obj.set_position(mol, mn.obj.get_attribute(mol, 'position') + 10)
+
+    pos_b = sample_attribute(mol, 'position')
+    
+    assert not np.isclose(pos_a, pos_b).all()
+    assert np.isclose(pos_a, pos_b - 10, rtol = 0.001).all()
