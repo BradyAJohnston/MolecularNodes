df7014977443b44ed113559a6e491e0a80ed7675
Johannes Elferich <jojotux123@hotmail.com>
2023-12-20 13:44:25 -0500

    Test should actually pass

--- tests/test_density.py
+++ tests/test_density.py
@@ -2,12 +2,15 @@ import molecularnodes as mn
 import pytest
 import numpy as np
 from .constants import test_data_directory
+try:
+    import pyopenvdb
+except ImportError:
+    pytest.skip("pyopenvdb not installed", allow_module_level=True)
 
 mn.unregister()
 mn.register()
 
 def test_density_load():
     file = test_data_directory / "emd_24805.map.gz"
-    obj = mn.io.density.load(file)
-    assert False
-   
+    obj = mn.io.density.load(file,style="density_surface")
+    assert obj.mn.molecule_type == "density"
