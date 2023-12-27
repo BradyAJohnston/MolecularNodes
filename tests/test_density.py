a8e59e4abee517575df18c093ac4164568c62dac
Johannes Elferich <jojotux123@hotmail.com>
2023-12-26 21:21:00 -0500

    Tests covering all options

--- tests/test_density.py
+++ tests/test_density.py
@@ -13,41 +13,77 @@ except ImportError:
 mn.unregister()
 mn.register()
 
-def test_density_load():
+@pytest.fixture
+def density_file():
     file = test_data_directory / "emd_24805.map.gz"
     vdb_file = test_data_directory / "emd_24805.vdb"
     vdb_file.unlink(missing_ok=True)
-    obj = mn.io.density.load(file,style="density_surface")
+    # Make sure this vdb file is not already loaded
+    mn.bpy.ops.wm.read_factory_settings()
+    return file
+
+def test_density_load(density_file):
+    
+    obj = mn.io.density.load(density_file,style="density_surface")
+    evaluated = mn.blender.obj.evaluate_using_debug_cube(obj)
+    pos = mn.blender.obj.get_attribute(evaluated,"position")
+
+    assert len(pos) > 1000
+
+    avg = np.mean(pos,axis=0)
+    assert np.linalg.norm(avg) > 0.5 
+
+    assert obj.mn.molecule_type == "density"
+    assert obj.users_collection[0] == mn.blender.coll.mn()
+
+def test_density_centered(density_file):
+
+    # First load using standar parameters to test recreation of vdb
+    mn.io.density.load(density_file,style="density_surface")
+    # Then refresh the scene
+    mn.bpy.ops.wm.read_factory_settings()
+
+    obj = mn.io.density.load(density_file,style="density_surface",center=True)
     evaluated = mn.blender.obj.evaluate_using_debug_cube(obj)
 
     pos = mn.blender.obj.get_attribute(evaluated,"position")
 
     assert len(pos) > 1000
-    # Get the average of positions
+
     avg = np.mean(pos,axis=0)
-    
-    assert np.linalg.norm(avg) > 0.1    
-    assert obj.mn.molecule_type == "density"
-    assert obj.users_collection[0] == mn.blender.coll.mn()
-
-def test_density_centered():
-    file = test_data_directory / "emd_24805.map.gz"
-    vdb_file = test_data_directory / "emd_24805.vdb"
-    vdb_file.unlink(missing_ok=True)
-    obj = mn.io.density.load(file,style="density_surface")
-    # Need to load an empty file to reset vdb file
-    mn.bpy.ops.wm.read_factory_settings()
-    obj2 = mn.io.density.load(file,style="density_surface",center=True)
-    evaluated = mn.blender.obj.evaluate_using_debug_cube(obj2)
-
-    pos = mn.blender.obj.get_attribute(evaluated,"position")
-
-    assert len(pos) > 1000
-    # Get the average of positions
-    avg = np.mean(pos,axis=0)
-    
     assert np.linalg.norm(avg) < 0.1
-    assert not np.allclose(avg,[0.0,0.0,0.0])
+
+def test_density_invert(density_file):
+
+    # First load using standar parameters to test recreation of vdb
+    mn.io.density.load(density_file,style="density_surface")
+    # Then refresh the scene
+    mn.bpy.ops.wm.read_factory_settings()
+
+    obj = mn.io.density.load(density_file,style="density_surface",invert=True)
+    style_node = mn.blender.nodes.get_style_node(obj)
+    style_node.inputs["Threshold"].default_value = 0.01
+    evaluated = mn.blender.obj.evaluate_using_debug_cube(obj)
+
+    pos = mn.blender.obj.get_attribute(evaluated,"position")    
+    # At this threshold after inverting we should have a cube the size of the volume
+    assert pos[:,0].max() > 2.0
+    assert pos[:,1].max() > 2.0
+    assert pos[:,2].max() > 2.0
+
+def test_density_normalize(density_file):
+
+    # First load using standar parameters to test recreation of vdb
+    mn.io.density.load(density_file,style="density_surface")
+    # Then refresh the scene
+    mn.bpy.ops.wm.read_factory_settings()
+
+    obj = mn.io.density.load(density_file,style="density_surface",normalize=True)
+    style_node = mn.blender.nodes.get_style_node(obj)
+    assert style_node.inputs["Threshold"].default_value == 0.25
+    evaluated = mn.blender.obj.evaluate_using_debug_cube(obj)
+    pos = mn.blender.obj.get_attribute(evaluated,"position")
+    assert len(pos) > 1000
 
 def test_density_multiple_load():
     file = test_data_directory / "emd_24805.map.gz"
