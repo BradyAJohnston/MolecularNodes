62825ac11339e9208154d3e5c8f1fd44493beb3e
Brady Johnston <brady.johnston@me.com>
2024-01-16 17:11:44 +0800

    add topology nodes
    final tweaks to docs and videos for topology nodes
    
    tweak not valid when both edge indices are the same
    
    fix getting of topology nodes
    
    some videos for new topology nodes
    
    change precision to 3 for platform mismatch
    
    cleanout tests
    
    finalise topology nodes
    
    still not confident that points of connected edges is working
    
    fix is_valid and add snapshots for topology nodes
    
    change data file back to 4.0 version
    
    add individual angle nodes
    
    cleanup DNA docs
    
    refactor node docs
    
    splits the node documentation into a different page for each category, with an overall landing page now in `nodes/index.qmd`.

--- molecularnodes/ui/func.py
+++ molecularnodes/ui/func.py
@@ -1,6 +1,7 @@
 import bpy
 from ..blender import nodes
 
+
 def build_menu(layout, items):
     for item in items:
         # print(item)
@@ -8,31 +9,37 @@ def build_menu(layout, items):
             layout.separator()
         elif item['label'] == "custom":
             for button in item['values']:
-                item['function'](layout, 
-                    label = button['label'], 
-                    field = button['field'], 
-                    prefix = button['prefix'], 
-                    property_id = button['property_id']
-                    )
+                item['function'](layout,
+                                 label=button['label'],
+                                 field=button['field'],
+                                 prefix=button['prefix'],
+                                 property_id=button['property_id']
+                                 )
         elif item['name'].startswith("mn."):
             layout.operator(item['name'])
         else:
-            menu_item_interface(layout, item['label'], item['name'], item['description'].removesuffix('.'))
+            label = item['label']
+            name = item['name']
+            description = item['description'].split('\n')[0].removesuffix('.')
+            menu_item_interface(layout, label=label,
+                                name=name, description=description)
 
-def menu_item_interface(layout_function, 
-                        label, 
-                        node_name, 
-                        node_description='Add custom MolecularNodes node group.', 
-                        node_link = False
+
+def menu_item_interface(layout_function,
+                        label,
+                        name,
+                        description='Add custom MolecularNodes node group.',
+                        node_link=False
                         ):
-    op = layout_function.operator('mn.add_custom_node_group', text = label)
+    op = layout_function.operator('mn.add_custom_node_group', text=label)
     op.node_label = label
-    op.node_name = node_name
-    op.node_description = node_description
+    op.node_name = name
+    op.node_description = description
     op.node_link = node_link
 
-def button_custom_color(layout, label, field, prefix, property_id, starting_value = 0):
-    op = layout.operator('mn.color_custom', text = label)
+
+def button_custom_color(layout, label, field, prefix, property_id, starting_value=0):
+    op = layout.operator('mn.color_custom', text=label)
     op.field = field
     op.prefix = prefix
     op.node_property = property_id
@@ -40,12 +47,12 @@ def button_custom_color(layout, label, field, prefix, property_id, starting_valu
     op.starting_value = starting_value
     op.description = f"Choose individual colors for each {label}"
 
-def button_custom_selection(layout, label, field, prefix, property_id, starting_value = 0):
-    op = layout.operator('mn.selection_custom', text = label)
+
+def button_custom_selection(layout, label, field, prefix, property_id, starting_value=0):
+    op = layout.operator('mn.selection_custom', text=label)
     op.field = field
     op.prefix = prefix
     op.node_property = property_id
     op.node_name = label.lower()
     op.starting_value = starting_value
     op.description = f"Create individual selections for each {label}"
-
