ed2baaf0ccad6dc7e3866ec557dd90a1a64ca944
Brady Johnston <brady.johnston@me.com>
2024-02-28 18:31:55 +0800

    update tests to include more attributes

--- tests/test_attributes.py
+++ tests/test_attributes.py
@@ -1,5 +1,9 @@
 import molecularnodes as mn
 import pytest
+import itertools
+import tempfile
+
+
 from .utils import sample_attribute_to_string
 from .constants import (
     codes,
@@ -9,12 +13,14 @@ from .constants import (
 mn.unregister()
 mn.register()
 
+formats = ['pdb', 'mmtf', 'cif']
 
-@pytest.mark.parametrize("code", codes)
-def test_attribute(snapshot, code, tmpdir):
-    mol = mn.io.fetch(code, cache_dir=tmpdir, style=None)
-    for attribute in attributes:
-        snapshot.assert_match(
-            sample_attribute_to_string(mol, attribute),
-            f"att_{attribute}_values.txt"
-        )
+with tempfile.TemporaryDirectory() as temp_dir:
+    @pytest.mark.parametrize("code, format", itertools.product(codes, formats))
+    def test_attribute(snapshot, code, format):
+        mol = mn.io.fetch(code, cache_dir=temp_dir, style=None, format=format)
+        for attribute in attributes:
+            snapshot.assert_match(
+                sample_attribute_to_string(mol, attribute),
+                f"att_{attribute}_values.txt"
+            )
