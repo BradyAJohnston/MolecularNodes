6af2a05028f7ea80265725df99c8133d6cf15251
Brady Johnston <brady.johnston@me.com>
2024-04-08 17:29:15 +0800

    return exit codes for tests

--- tests/run.py
+++ tests/run.py
@@ -9,7 +9,11 @@ argv = argv[argv.index("--") + 1:]
 # /Applications/Blender.app/Contents/MacOS/Blender -b -P tests/run.py -- . -k test_color_lookup_supplied
 
 def main():
-    pytest.main(argv)
+    # run the test suite, and we have to manually return the result value if non-zero
+    # value is returned for a failing test
+    result = pytest.main(argv)
+    if result.value != 0:
+        sys.exit(result.value)
 
 
 if __name__ == "__main__":
