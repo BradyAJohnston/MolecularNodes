ee10c5fd523ac95d9e054e0cbed58d6d9bec3031
Brady Johnston <brady.johnston@me.com>
2024-05-03 11:30:03 +0800

    maybe fix?

--- tests/utils.py
+++ tests/utils.py
@@ -17,32 +17,21 @@ class NumpySnapshotExtension(AmberSnapshotExtension):
         return super().serialize(data)
 
     def assert_match(self, snapshot, test_value):
-        if isinstance(test_value, np.ndarray) and isinstance(snapshot, list):
+        if isinstance(test_value, np.ndarray):
             # if the values are floats, then we use a rough "isclose" to compare them
             # which helps with floating point issues. Between platforms geometry nodes
             # outputs some differences in the meshes which are usually off by ~0.01 or so
             if np.issubdtype(test_value, float):
                 assert np.allclose(
-                    test_value, np.array(snapshot), rtol=0.05
+                    test_value,
+                    np.array(snapshot),
+                    rtol=0.05
                 ).all()
             else:
                 assert (test_value == np.array(snapshot)).all()
 
         else:
             super().assert_match(snapshot, test_value)
-
-
-def apply_mods(obj):
-    """
-    Applies the modifiers on the modifier stack
-
-    This will realise the computations inside of any Geometry Nodes modifiers, ensuring
-    that the result of the node trees can be compared by looking at the resulting 
-    vertices of the object.
-    """
-    bpy.context.view_layer.objects.active = obj
-    for modifier in obj.modifiers:
-        bpy.ops.object.modifier_apply(modifier=modifier.name)
 
 
 def sample_attribute(object,
@@ -126,74 +115,6 @@ def sample_attribute_to_string(object,
         array = attribute[idx, :]
 
     return np.array2string(array, precision=precision, threshold=threshold)
-
-
-def get_verts(obj, float_decimals=4, n_verts=100, apply_modifiers=True, seed=42):
-    """
-    Randomly samples a specified number of vertices from an object.
-
-    Parameters
-    ----------
-    obj : object
-        Object from which to sample vertices.
-    float_decimals : int, optional
-        Number of decimal places to round the vertex coordinates, defaults to 4.
-    n_verts : int, optional
-        Number of vertices to sample, defaults to 100.
-    apply_modifiers : bool, optional
-        Whether to apply all modifiers on the object before sampling vertices, defaults to True.
-    seed : int, optional
-        Seed for the random number generator, defaults to 42.
-
-    Returns
-    -------
-    str
-        String representation of the randomly selected vertices.
-
-    Notes
-    -----
-    This function randomly samples a specified number of vertices from the given object.
-    By default, it applies all modifiers on the object before sampling vertices. The
-    random seed can be set externally for reproducibility.
-
-    If the number of vertices to sample (`n_verts`) exceeds the number of vertices
-    available in the object, all available vertices will be sampled.
-
-    The vertex coordinates are rounded to the specified number of decimal places
-    (`float_decimals`) before being included in the output string.
-
-    Examples
-    --------
-    >>> obj = mn.io.fetch.('6n2y', style='cartoon')
-    >>> get_verts(obj, float_decimals=3, n_verts=50, apply_modifiers=True, seed=42)
-    '1.234,2.345,3.456\n4.567,5.678,6.789\n...'
-    """
-
-    import random
-
-    random.seed(seed)
-
-    if apply_modifiers:
-        try:
-            apply_mods(obj)
-        except RuntimeError as ex:
-            return str(ex)
-
-    vert_list = [(v.co.x, v.co.y, v.co.z) for v in obj.data.vertices]
-
-    if n_verts > len(vert_list):
-        n_verts = len(vert_list)
-
-    random_verts = random.sample(vert_list, n_verts)
-
-    verts_string = ""
-    for i, vert in enumerate(random_verts):
-        if i < n_verts:
-            rounded = [round(x, float_decimals) for x in vert]
-            verts_string += "{},{},{}\n".format(
-                rounded[0], rounded[1], rounded[2])
-
-    return verts_string
 
 
 def remove_all_molecule_objects(mda_session):
