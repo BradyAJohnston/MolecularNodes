21e6c6d92fec21cbb84e40bb8e208a5f57a81cd2
Brady Johnston <36021261+BradyAJohnston@users.noreply.github.com>
2024-07-11 17:26:13 +0800

    Template Install Button (#544)
    * fix preferences panel and add buttons to install and uninstall template
    
    * tweak install and uninstall
    
    * cleaned up install function
    
    * cleanupd
    
    * fix test
    
    * refactor startup template file

--- molecularnodes/ui/pref.py
+++ molecularnodes/ui/pref.py
@@ -1,27 +1,54 @@
 import bpy
-from bpy.types import AddonPreferences
 
-from ..utils import template_install
-
-install_instructions = "https://bradyajohnston.github.io/MolecularNodes/installation.html#installing-biotite-mdanalysis"
+from .. import __package__, template
 
 
-class MN_OT_Install_Template(bpy.types.Operator):
-    bl_idname = "mn.install_template"
-    bl_label = "Install Template File"
+class MN_OT_Template_Install(bpy.types.Operator):
+    bl_idname = "mn.template_install"
+    bl_label = "Install Template"
     bl_description = "Install the Molecular Nodes startup template file."
 
-    def exectute(self, context):
-        template_install()
+    def execute(self, context):
+        template.install()
+        self.report({"INFO"}, "Installed Molecular Nodes template.")
+        return {"FINISHED"}
 
 
-class MolecularNodesPreferences(AddonPreferences):
+class MN_OT_Template_Uninstall(bpy.types.Operator):
+    bl_idname = "mn.template_uninstall"
+    bl_label = "Uninstall Template"
+    bl_description = "Uninstall the Molecular Nodes startup template file."
+
+    @classmethod
+    def poll(cls, context):
+        return template.is_installed()
+
+    def execute(self, context):
+        try:
+            template.uninstall()
+            self.report({"INFO"}, "Uninstalled Molecular Nodes template.")
+        except FileNotFoundError:
+            self.report({"WARNING"}, "Template not installed.")
+
+        return {"FINISHED"}
+
+
+class MolecularNodesPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="testing")
-        layout.operator("mn.install_template", text="Install Template")
+        layout.label(
+            text="Install the Molecular Nodes template file, to start Blender with useful default settings"
+        )
+        row = layout.row()
+        if not template.is_installed():
+            text = "Install Template"
+        else:
+            text = "Reinstall Template"
+
+        row.operator("mn.template_install", text=text)
+        row.operator("mn.template_uninstall")
 
 
-CLASSES = [MN_OT_Install_Template, MolecularNodesPreferences]
+CLASSES = [MN_OT_Template_Install, MN_OT_Template_Uninstall, MolecularNodesPreferences]
