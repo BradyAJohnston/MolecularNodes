6ff08470e154ab6d3fa929c47520b0d285444fd1
Brady Johnston <brady.johnston@me.com>
2024-11-28 18:01:53 +1100

    handle new oxdna topology inclduing custom

--- molecularnodes/entities/trajectory/oxdna/OXDNAParser.py
+++ molecularnodes/entities/trajectory/oxdna/OXDNAParser.py
@@ -17,30 +17,103 @@ class OXDNAParser(TopologyReaderBase):
 
         return top
 
-    def _parseatoms(self):
-        with open(self.filename) as f:
+    @classmethod
+    def _is_new_topology(self, filename) -> bool:
+        with open(filename) as f:
+            return "5->3" in f.readline()
+
+    @classmethod
+    def _read_topo_new(cls, filename) -> Topology:
+        with open(filename) as f:
+            lines = f.readlines()
+        n_atoms, n_chains, direction = np.array(lines[0].split())
+        n_atoms = int(n_atoms)
+        atom_idx = np.arange(n_atoms)
+
+        res_name_list = []
+        chain_id_list = []
+        for i, line in enumerate(lines[1:]):
+            is_rna = "type=RNA" in line
+            is_dna = not is_rna
+            contains_custom_base = "(" in line and ")" in line
+
+            line_split = line.split()
+            bases = line_split[0]
+
+            chain_id_list.append(np.repeat(i, len(bases)))
+
+            base_list = []
+            start = 0
+            end = 0
+            in_custom_base = False
+            for i, letter in enumerate(bases):
+                if letter == "(":
+                    start = i + 1
+                    in_custom_base = True
+                    continue
+                if letter == ")":
+                    end = i
+                    base_list.append(line[start:end])
+                    in_custom_base = False
+                    continue
+
+                if in_custom_base:
+                    continue
+
+                base_list.append(letter)
+
+            res_name_list.append(np.array(base_list))
+
+        res_names = np.hstack(res_name_list)
+        chain_ids = np.hstack(chain_id_list)
+
+        bond_idx = np.zeros((n_atoms, 2), dtype=int)
+        bond_idx[:, :] = -1
+
+        for i in atom_idx:
+            if i == 1:
+                continue
+            if chain_ids[i] == chain_ids[-1]:
+                bond_idx[i, :] = np.array((i, i - 1), dtype=int)
+
+        mask = np.logical_and(bond_idx[:, 0] != -1, bond_idx[:, 1] != -1)
+        bond_idx = bond_idx[mask, :]
+
+        attrs = []
+        for Attr in (Atomids, Resnums, Resids):
+            attrs.append(Attr(atom_idx))
+
+        attrs.append(ChainIDs(chain_ids))
+        attrs.append(Resnames(res_names))
+
+        topo = Topology(n_atoms, n_atoms, 1, attrs=attrs)
+        bonds = Bonds(bond_idx)
+        topo.add_TopologyAttr(bonds)
+
+        return topo
+
+    @classmethod
+    def _read_topo_old(cls, filename) -> Topology:
+        with open(filename) as f:
             first_line = f.readline()
 
-        is_new_topology = "5->3" in first_line
+        dimensions = np.array(first_line.split())
+        n_atoms = int(dimensions[0])
+        array = np.loadtxt(filename, skiprows=1, max_rows=n_atoms, dtype=str)
 
-        if not is_new_topology:
-            dimensions = np.array(first_line.split(), dtype=int)
-            n_atoms = dimensions[0]
-            array = np.loadtxt(self.filename, skiprows=1, max_rows=n_atoms, dtype=str)
+        # each topology item has two bond columns, which say what the base is bonded
+        # from and what it is bonded to. -1 means it is not bonded
+        # we need to turn that into an array of bond pairs
+        bond_idx = np.zeros((n_atoms * 2, 2), int)
+        row_numbers = np.arange(n_atoms, dtype=int)
+        for i in range(2):
+            rows = row_numbers + (i * n_atoms)
+            bond_idx[rows, 0] = array[:, (i + 2)]
+            bond_idx[rows, 1] = row_numbers
 
-            # each topology item has two bond columns, which say what the base is bonded
-            # from and what it is bonded to. -1 means it is not bonded
-            # we need to turn that into an array of bond pairs
-            bond_idx = np.zeros((n_atoms * 2, 2), int)
-            row_numbers = np.arange(n_atoms, dtype=int)
-            for i in range(2):
-                rows = row_numbers + (i * n_atoms)
-                bond_idx[rows, 0] = array[:, (i + 2)]
-                bond_idx[rows, 1] = row_numbers
-
-            # drop any that have -1 as they aren't bonded to anything
-            mask = np.logical_and(bond_idx[:, 0] != -1, bond_idx[:, 1] != -1)
-            bond_idx = bond_idx[mask, :]
+        # drop any that have -1 as they aren't bonded to anything
+        mask = np.logical_and(bond_idx[:, 0] != -1, bond_idx[:, 1] != -1)
+        bond_idx = bond_idx[mask, :]
 
         attrs = []
         idx = np.arange(1, n_atoms + 1, dtype=int)
@@ -55,3 +128,9 @@ class OXDNAParser(TopologyReaderBase):
         topo.add_TopologyAttr(bonds)
 
         return topo
+
+    def _parseatoms(self):
+        if self._is_new_topology(self.filename):
+            return self._read_topo_new(self.filename)
+        else:
+            return self._read_topo_old(self.filename)
--- tests/test_dna.py
+++ tests/test_dna.py
@@ -11,6 +11,18 @@ class TestOXDNAReading:
         return data_dir / "oxdna/holliday.top"
 
     @pytest.fixture(scope="module")
+    def filepath_top_new(self):
+        return data_dir / "oxdna/top_new.top"
+
+    @pytest.fixture(scope="module")
+    def filepath_top_new_custom(self):
+        return data_dir / "oxdna/top_new_custom.top"
+
+    @pytest.fixture(scope="module")
+    def filepath_top_old(self):
+        return data_dir / "oxdna/top_old.top"
+
+    @pytest.fixture(scope="module")
     def filepath_traj_dat(self):
         return data_dir / "oxdna/holliday.dat"
 
@@ -43,3 +55,25 @@ class TestOXDNAReading:
         traj.create_object()
         for name in ["position", "res_name", "res_id", "chain_id"]:
             assert snapshot_custom == getattr(traj, name)
+
+    def test_detect_new_top(
+        self, filepath_top_old, filepath_top_new, filepath_top_new_custom
+    ):
+        assert dna.OXDNAParser._is_new_topology(filepath_top_new)
+        assert dna.OXDNAParser._is_new_topology(filepath_top_new_custom)
+        assert not dna.OXDNAParser._is_new_topology(filepath_top_old)
+
+    def test_topo_reading(
+        self,
+        snapshot,
+        filepath_top_old,
+        filepath_top_new,
+        filepath_top_new_custom,
+    ):
+        top_new = dna.OXDNAParser._read_topo_new(filepath_top_new)
+        top_new_custom = dna.OXDNAParser._read_topo_new(filepath_top_new_custom)
+        top_old = dna.OXDNAParser._read_topo_old(filepath_top_old)
+
+        for top in [top_new, top_old, top_new_custom]:
+            for att in ("resnames", "resids"):
+                assert snapshot == getattr(top, att)
