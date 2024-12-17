201ccf6cc7b9ea1bf47cab1606209ef7596e9959
Brady Johnston <brady.johnston@me.com>
2024-12-17 14:09:05 +0800

    initial pretty filters for socket data types

---
+++ docs/filters.lua
@@ -0,0 +1,18 @@
+local keywords = {"Float", "Int", "Vector", "Geometry", "Bool", "Matrix", "Rotation", "Material", "Color", "Collection"}
+
+function Code(el)
+  for _, keyword in ipairs(keywords) do
+    if el.text == keyword then
+      table.insert(el.classes, "custom-" .. keyword:lower())
+      return el
+    end
+  end
+end
+
+-- function Str(el)
+--   for _, keyword in ipairs(keywords) do
+--     if el.text == keyword then
+--       return pandoc.Span(el.text, pandoc.Attr("", {"custom-" .. keyword:lower()}, {}))
+--     end
+--   end
+-- end
