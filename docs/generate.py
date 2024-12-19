ba20a7f765eecc4b825680eb0fd21b24b49567d4
Brady Johnston <brady.johnston@me.com>
2024-12-19 15:12:04 +0800

    data_table.qmd generation

--- docs/generate.py
+++ docs/generate.py
@@ -17,6 +17,7 @@ fig-align: center
 ---
 """
 
+# generate all of the node related docs
 for submenu in mn.ui.node_menu.menu_items.submenus:
     with open(DOCS_FOLDER / f"nodes/{submenu.name}.qmd", "w") as file:
         file.write(header)
@@ -42,3 +43,27 @@ for submenu in mn.ui.node_menu.menu_items.submenus:
 
             file.write(doc.as_markdown())
             file.write("\n\n")
+
+
+# write the data table page
+with open(DOCS_FOLDER / "data_table.qmd", "w") as file:
+    file.write(header)
+    file.write("# Data Tables\n\n")
+    file.write(
+        "The different lookup tables that are used to conver strings to integers in Molecular Nodes.\n\n"
+        "Code for this can be found on the [GitHub Page](https://github.com/BradyAJohnston/MolecularNodes/blob/main/molecularnodes/data.py)\n\n"
+    )
+    file.write(
+        "### Residue Names\n\n" "| Name | Integer |\n" "|----------:|:------------|\n"
+    )
+    for name, res in mn.data.residues.items():
+        file.write(f"| {name} | `{res['res_name_num']}::Int` |\n")
+    file.write("\n")
+    file.write("\n")
+
+    file.write(
+        "### Atom Names\n\n" "| Name | Integer |\n" "|----------:|:------------|\n"
+    )
+    for name, value in mn.data.atom_names.items():
+        file.write(f"| {name} | `{value}::Int` |\n")
+    file.write("\n")
