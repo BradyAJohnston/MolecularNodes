cb988b1439d936cac21b8a5c10eb1d72f95a6430
Brady Johnston <brady.johnston@me.com>
2025-04-05 10:52:21 +0800

    use 4.4 version of data file

--- molecularnodes/assets/__init__.py
+++ molecularnodes/assets/__init__.py
@@ -4,7 +4,7 @@ from .template import install, uninstall
 
 ASSET_DIR = Path(__file__).resolve().parent
 ADDON_DIR = ASSET_DIR.parent
-MN_DATA_FILE = ASSET_DIR / "MN_data_file_4.2.blend"
+MN_DATA_FILE = ASSET_DIR / "MN_data_file_4.4.blend"
 
 
 __all__ = ["data", "install", "uninstall", "MN_DATA_FILE"]
