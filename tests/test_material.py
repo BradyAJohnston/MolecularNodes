e069422a1471deb15e25623bcffa747d4d8366d4
zachcp <zachcp@users.noreply.github.com>
2025-04-29 19:26:33 -0400

    Testing for Used-Defined Materials (#843)
    * begin testing for arbitrary materials
    
    * update tests for user materials
    
    * ruff

--- tests/test_material.py
+++ tests/test_material.py
@@ -21,6 +21,44 @@ def test_setting_material():
         s.material.mix_a
 
 
+def test_setting_material_in_apply_builtins():
+    materials = [
+        mn.material.AmbientOcclusion(),
+        mn.material.Default(),
+        mn.material.FlatOutline(),
+        mn.material.Squishy(),
+        mn.material.TransparentOutline(),
+    ]
+    for material in materials:
+        mol = mn.Molecule.fetch("4ozs").add_style("cartoon", material=material)
+        assert mol is not None, f"Failed to apply {material.__class__.__name__}"
+
+
+def test_setting_material_in_apply_custom():
+    def create_new_material(name="New_Material", color=(1, 0, 0, 1)):
+        if name in bpy.data.materials:
+            material = bpy.data.materials[name]
+        else:
+            material = bpy.data.materials.new(name=name)
+        material.use_nodes = True
+        material.node_tree.nodes.clear()
+        principled_node = material.node_tree.nodes.new("ShaderNodeBsdfPrincipled")
+        principled_node.location = (0, 0)
+        principled_node.inputs["Base Color"].default_value = color
+        principled_node.inputs["Metallic"].default_value = 0.5
+        principled_node.inputs["Roughness"].default_value = 0.2
+        output_node = material.node_tree.nodes.new("ShaderNodeOutputMaterial")
+        output_node.location = (300, 0)
+        material.node_tree.links.new(
+            principled_node.outputs["BSDF"], output_node.inputs["Surface"]
+        )
+        return material
+
+    new_mat = create_new_material(name="My_Red_Material", color=(1, 0, 0, 1))
+    mol = mn.Molecule.fetch("4ozs").add_style("cartoon", material=new_mat)
+    assert mol is not None
+
+
 def test_generic_material():
     mol = mn.Molecule.fetch("4ozs").add_style("cartoon")
     s = mol.styles[0]
