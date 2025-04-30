4cefae09f8f08fdb547ae703bda6edfa6d3ed24d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-04-30 11:05:50 -0400

    pass kwargs to different style types

--- molecularnodes/nodes/styles.py
+++ molecularnodes/nodes/styles.py
@@ -1,117 +1,56 @@
-"""Constant style definitions for molecular visualization in Blender.
-
-This module contains predefined style settings for different molecular visualization
-modes including ball-and-stick, cartoon, ribbon, spheres, sticks and surface
-representations.
-
-"""
-
-from dataclasses import dataclass, replace, field, fields
-from typing import List, Tuple, Union
-
-
-@dataclass(frozen=True)
-class StyleBase:
-    def get_by_key(self, original_key: str):
-        for f in fields(self):
-            if f.metadata.get('key') == original_key:
-                return getattr(self, f.name)
-        return None
-
-    def update_properties(self, **changes):
-        return replace(self, **changes)
-
-@dataclass(frozen=True)
-class BallStickStyle(StyleBase):
-    style: str = field(default="ball+stick", metadata={"key": "Style"})
-    quality: int = field(default=2, metadata={"key": "Quality"})
-    as_mesh: bool = field(default=True, metadata={"key": "As Mesh"})
-    sphere_radii: float = field(default=0.3, metadata={"key": "Sphere Radii"})
-    bond_split: bool = field(default=False, metadata={"key": "Bond Split"})
-    bond_find: bool = field(default=True, metadata={"key": "Bond Find"})
-    bond_radius: float = field(default=0.3, metadata={"key": "Bond Radius"})
-    color_blur: bool = field(default=False, metadata={"key": "Color Blur"})
-    shade_smooth: bool = field(default=True, metadata={"key": "Shade Smooth"})
-
-
-@dataclass(frozen=True)
-class CartoonStyle(StyleBase):
-    style: str = field(default="cartoon", metadata={"key": "Style"})
-    quality: int = field(default=2, metadata={"key": "Quality"})
-    dssp: bool = field(default=False, metadata={"key": "DSSP"})
-    cylinders: bool = field(default=False, metadata={"key": "Cylinders"})
-    arrows: bool = field(default=True, metadata={"key": "Arrows"})
-    rounded: bool = field(default=False, metadata={"key": "Rounded"})
-    thickness: float = field(default=0.6, metadata={"key": "Thickness"})
-    width: float = field(default=2.2, metadata={"key": "Width"})
-    loop_radius: float = field(default=0.3, metadata={"key": "Loop Radius"})
-    smoothing: float = field(default=0.5, metadata={"key": "Smoothing"})
-    color_blur: bool = field(default=False, metadata={"key": "Color Blur"})
-    shade_smooth: bool = field(default=True, metadata={"key": "Shade Smooth"})
-
-
-@dataclass(frozen=True)
-class RibbonStyle(StyleBase):
-    style: str = field(default="ribbon", metadata={"key": "Style"})
-    quality: int = field(default=3, metadata={"key": "Quality"})
-    radius: float = field(default=1.6, metadata={"key": "Radius"})
-    smoothing: float = field(default=0.6, metadata={"key": "Smoothing"})
-    color_blur: bool = field(default=False, metadata={"key": "Color Blur"})
-    shade_smooth: bool = field(default=False, metadata={"key": "Shade Smooth"})
-
-@dataclass(frozen=True)
-class SpheresStyle(StyleBase):
-    style: str = field(default="sphere", metadata={"key": "Style"})
-    as_mesh: bool = field(default=True, metadata={"key": "Sphere As Mesh"})
-    radii: float = field(default=0.8, metadata={"key": "Sphere Radii"})
-    subdivisions: int = field(default=2, metadata={"key": "Sphere Subdivisions"})
-    shade_smooth: bool = field(default=False, metadata={"key": "Shade Smooth"})
-
-
-@dataclass(frozen=True)
-class SticksStyle(StyleBase):
-    style: str = field(default="sticks", metadata={"key": "Style"})
-    quality: int = field(default=2, metadata={"key": "Quality"})
-    radius: float = field(default=0.2, metadata={"key": "Radius"})
-    color_blur: bool = field(default=False, metadata={"key": "Color Blur"})
-    shade_smooth: bool = field(default=False, metadata={"key": "Shade Smooth"})
-
-
-@dataclass(frozen=True)
-class SurfaceStyle(StyleBase):
-    style: str = field(default="surface", metadata={"key": "Style"})
-    quality: int = field(default=3, metadata={"key": "Quality"})
-    separate: bool = field(default=True, metadata={"key": "Separate"})
-    attribute: str = field(default="chain_id", metadata={"key": "Attribute"})
-    scale_radii: float = field(default=1.5, metadata={"key": "Scale Radii"})
-    probe_size: float = field(default=1.0, metadata={"key": "Probe Size"})
-    triangulate: bool = field(default=False, metadata={"key": "Triangulate"})
-    relaxation_steps: int = field(default=10, metadata={"key": "Relaxation Steps"})
-    by_ca: bool = field(default=False, metadata={"key": "by CA"})
-    blur: int = field(default=2, metadata={"key": "Blur"})
-    shade_smooth: bool = field(default=True, metadata={"key": "Shade Smooth"})
-
-
-StyleType = Union[BallStickStyle, CartoonStyle, RibbonStyle, SpheresStyle, SticksStyle, SurfaceStyle]
-
-class StyleCreator():
-    def new() -> StyleType:
-        return CartoonStyle()
-
-    def ball_stick() -> BallStickStyle:
-        return BallStickStyle()
-
-    def cartoon() -> CartoonStyle:
-        return CartoonStyle()
-
-    def ribbon() -> RibbonStyle:
-        return RibbonStyle()
-
-    def spheres() -> SpheresStyle:
-        return SpheresStyle()
-
-    def sticks() -> SticksStyle:
-        return SticksStyle()
-
-    def surface() -> SurfaceStyle:
-        return SurfaceStyle()
+STYLE_DATA = {
+    "Style Ball and Stick": [
+        { "name": "quality", "blendername": "Quality", "type": int, "default": 2 },
+        { "name": "as_mesh", "blendername": "As Mesh", "type": bool, "default": True },
+        { "name": "sphere_radii", "blendername": "Sphere Radii", "type": float, "default": 0.3 },
+        { "name": "bond_split", "blendername": "Bond Split", "type": bool, "default": False },
+        { "name": "bond_find", "blendername": "Bond Find", "type": bool, "default": True },
+        { "name": "bond_radius", "blendername": "Bond Radius", "type": float, "default": 0.3 },
+        { "name": "color_blur", "blendername": "Color Blur", "type": bool, "default": False },
+        { "name": "shade_smooth", "blendername": "Shade Smooth", "type": bool, "default": True }
+    ],
+    "Style Cartoon": [
+        { "name": "quality", "blendername": "Quality", "type": int, "default": 2 },
+        { "name": "dssp", "blendername": "DSSP", "type": bool, "default": False },
+        { "name": "cylinders", "blendername": "Cylinders", "type": bool, "default": False },
+        { "name": "arrows", "blendername": "Arrows", "type": bool, "default": True },
+        { "name": "rounded", "blendername": "Rounded", "type": bool, "default": False },
+        { "name": "thickness", "blendername": "Thickness", "type": float, "default": 0.6 },
+        { "name": "width", "blendername": "Width", "type": float, "default": 2.2 },
+        { "name": "loop_radius", "blendername": "Loop Radius", "type": float, "default": 0.3 },
+        { "name": "smoothing", "blendername": "Smoothing", "type": float, "default": 0.5 },
+        { "name": "color_blur", "blendername": "Color Blur", "type": bool, "default": False },
+        { "name": "shade_smooth", "blendername": "Shade Smooth", "type": bool, "default": True }
+    ],
+    "Style Ribbon": [
+        { "name": "quality", "blendername": "Quality", "type": int, "default": 3 },
+        { "name": "radius", "blendername": "Radius", "type": float, "default": 1.6 },
+        { "name": "smoothing", "blendername": "Smoothing", "type": float, "default": 0.6 },
+        { "name": "color_blur", "blendername": "Color Blur", "type": bool, "default": False },
+        { "name": "shade_smooth", "blendername": "Shade Smooth", "type": bool, "default": False }
+    ],
+    "Style Spheres": [
+        { "name": "as_mesh", "blendername": "Sphere As Mesh", "type": bool, "default": True },
+        { "name": "radii", "blendername": "Sphere Radii", "type": float, "default": 0.8 },
+        { "name": "subdivisions", "blendername": "Sphere Subdivisions", "type": int, "default": 2 },
+        { "name": "shade_smooth", "blendername": "Shade Smooth", "type": bool, "default": False }
+    ],
+    "Style Sticks": [
+        { "name": "quality", "blendername": "Quality", "type": int, "default": 2 },
+        { "name": "radius", "blendername": "Radius", "type": float, "default": 0.2 },
+        { "name": "color_blur", "blendername": "Color Blur", "type": bool, "default": False },
+        { "name": "shade_smooth", "blendername": "Shade Smooth", "type": bool, "default": False }
+    ],
+    "Style Sruface": [
+        { "name": "quality", "blendername": "Quality", "type": int, "default": 3 },
+        { "name": "separate", "blendername": "Separate", "type": bool, "default": True },
+        { "name": "attribute", "blendername": "Attribute", "type": str, "default": "chain_id" },
+        { "name": "scale_radii", "blendername": "Scale Radii", "type": float, "default": 1.5 },
+        { "name": "probe_size", "blendername": "Probe Size", "type": float, "default": 1.0 },
+        { "name": "triangulate", "blendername": "Triangulate", "type": bool, "default": False },
+        { "name": "relaxation_steps", "blendername": "Relaxation Steps", "type": int, "default": 10 },
+        { "name": "by_ca", "blendername": "by CA", "type": bool, "default": False },
+        { "name": "blur", "blendername": "Blur", "type": int, "default": 2 },
+        { "name": "shade_smooth", "blendername": "Shade Smooth", "type": bool, "default": True }
+    ]
+}
