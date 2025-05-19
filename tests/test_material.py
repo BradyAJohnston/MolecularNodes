d884744806871a1dd8c236470a01369e72a9d20d
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-05-19 08:59:37 -0400

    test fix

--- tests/test_material.py
+++ tests/test_material.py
@@ -60,7 +60,7 @@ def test_setting_material_in_apply_custom():
 
 
 def test_material_creator():
-    pearl_mat = mn.material.MN_Materials.pearl()
+    pearl_mat = mn.material.Materials.pearl()
     assert isinstance(pearl_mat, bpy.types.Material)
 
 
