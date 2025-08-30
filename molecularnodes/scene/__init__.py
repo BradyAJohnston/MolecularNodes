137c088edaf3b32fef7dce278e295ae4ec4d09af
Brady Johnston <brady.johnston@me.com>
2025-08-30 08:49:01 +0100

    more improvements to scripting

--- molecularnodes/scene/__init__.py
+++ molecularnodes/scene/__init__.py
@@ -1,4 +1,4 @@
-from .base import Canvas
+from .base import Canvas, ViewTransform
 from .engines import EEVEE, Cycles
 
-__all__ = ["Canvas", "EEVEE", "Cycles"]
+__all__ = ["Canvas", "EEVEE", "Cycles", ViewTransform]
