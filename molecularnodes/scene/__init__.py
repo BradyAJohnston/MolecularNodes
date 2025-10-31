43dc7a5dbab270238b210fd9d5cd8b9f601f9ffe
Brady Johnston <brady.johnston@me.com>
2025-10-31 11:30:27 -0700

    fix some doc issues

--- molecularnodes/scene/__init__.py
+++ molecularnodes/scene/__init__.py
@@ -1,4 +1,4 @@
 from .base import Canvas, ViewTransform
 from .engines import EEVEE, Cycles
 
-__all__ = ["Canvas", "EEVEE", "Cycles", ViewTransform]
+__all__ = ["Canvas", "EEVEE", "Cycles", "ViewTransform"]
