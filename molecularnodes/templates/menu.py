56ddf2e349fc0f022284d328247f7bf7c04fa280
Pardhav Maradani <pardhav.maradani@gmail.com>
2025-12-11 20:18:53 +0530

    * Switch to flat menu with labels

--- molecularnodes/templates/menu.py
+++ molecularnodes/templates/menu.py
@@ -1,26 +1,10 @@
 from pathlib import Path
 import bpy
 
-# List any new subdirs here
-_subdirs = ["api", "annotations"]
-
-
-def _get_submenu_idname(name):
-    """Return unique bl_idname for a submenu item"""
-    return f"MN_MT_templates_{name}"
-
-
-def _create_dynamic_menu_class(subdir):
-    """Create a dynamic menu class"""
-    path = Path(__file__).parent / subdir
-
-    def draw(self, context):
-        self.path_menu(
-            searchpaths=[path], operator="text.open", props_default={"internal": True}
-        )
-
-    attributes = dict(bl_idname=_get_submenu_idname(subdir), bl_label=subdir, draw=draw)
-    return type(subdir, (bpy.types.Menu,), attributes)
+_folder_map = {
+    "API": "api",
+    "Annotations": "annotations",
+}
 
 
 class MN_MT_templates(bpy.types.Menu):
@@ -28,8 +12,21 @@ class MN_MT_templates(bpy.types.Menu):
     bl_label = "Molecular Nodes"
 
     def draw(self, context):
-        for subdir in _subdirs:
-            self.layout.menu(_get_submenu_idname(subdir), text=subdir.title())
+        layout: bpy.types.UILayout = self.layout
+        assert layout is not None
+
+        for i, (label, folder) in enumerate(_folder_map.items()):
+            path = Path(__file__).parent / folder
+            if not path.is_dir():
+                continue
+            if i > 0:
+                layout.separator()
+            layout.label(text=label)
+            self.path_menu(
+                searchpaths=[path],
+                operator="text.open",
+                props_default={"internal": True},
+            )
 
 
 def _draw_templates_menu(self, context):
@@ -38,25 +35,10 @@ def _draw_templates_menu(self, context):
 
 
 def register_templates_menu():
-    # register submenu classes
-    for cls in _submenu_classes:
-        bpy.utils.register_class(cls)
-    # register main menu class
     bpy.utils.register_class(MN_MT_templates)
-    # add to Text Editor > Templates menu
     bpy.types.TEXT_MT_templates.append(_draw_templates_menu)
 
 
 def unregister_templates_menu():
-    # remove from Text Editor > Templates menu
     bpy.types.TEXT_MT_templates.remove(_draw_templates_menu)
-    # unregister main menu class
     bpy.utils.unregister_class(MN_MT_templates)
-    # unregister submenu classes
-    for cls in reversed(_submenu_classes):
-        bpy.utils.unregister_class(cls)
-
-
-_submenu_classes = []
-for subdir in _subdirs:
-    _submenu_classes.append(_create_dynamic_menu_class(subdir))
