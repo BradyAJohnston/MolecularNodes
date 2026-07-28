17431ada8437a0b8eafa059a930eb29b360c9084
Rich <rich@waldo.town>
2026-07-28 11:54:32 -0700

    Pull field creation into its own function

--- molecularnodes/entities/ensemble/cryosparc.py
+++ molecularnodes/entities/ensemble/cryosparc.py
@@ -1,8 +1,15 @@
 from pathlib import Path
+from typing import TYPE_CHECKING, overload
 import databpy as db
 import numpy as np
 from scipy.spatial.transform import Rotation
 from .base import Ensemble, EntityType
+
+if TYPE_CHECKING:
+    from typing import Any, Literal, TypeVar
+
+    type BobField = Literal["data", "name", "atype", "domain"]
+    F = TypeVar("F")
 
 
 class Dataset:
@@ -20,9 +27,11 @@ class Dataset:
     def __len__(self) -> int:
         return len(self.dset)
 
-    def get(
-        self, key: str, fallback: np.typing.NDArray | None = None
-    ) -> np.typing.NDArray | None:
+    @overload
+    def get(self, key: str, fallback: None = None) -> np.typing.NDArray | None: ...
+    @overload
+    def get(self, key: str, fallback: np.typing.NDArray) -> np.typing.NDArray: ...
+    def get(self, key, fallback=None):
         if key in self:
             return self[key]
         else:
@@ -69,29 +78,28 @@ class Dataset:
             return shifts * self.psize_3d.reshape(len(self), -1)
 
     @property
-    def positions(self) -> np.ndarray[tuple[int, int], np.dtype[np.float32]]:
-        """
-        Particle positions in micrograph fractions (X, Y).
-        """
-        xpos = self.get("location/center_x_frac")
-        ypos = self.get("location/center_y_frac")
-        if xpos is None or ypos is None:
-            return np.zeros((len(self), 2), np.float32)
-        return np.column_stack([xpos, ypos])
-
-    @property
-    def defocus(self) -> np.ndarray[tuple[int], np.dtype[np.float32]]:
+    def defocus(self) -> np.ndarray[tuple[int, int], np.dtype[np.float32]]:
         """
         Defocus in microns.
         """
         df1 = self.get("ctf/df1_A")
         df2 = self.get("ctf/df2_A")
         if df1 is None or df2 is None:
-            return np.zeros(len(self), np.float32)
-        return np.mean(np.column_stack([df1, df2]) / 10_000, axis=1)
+            return np.zeros((len(self), 2), np.float32)
+        return np.column_stack([df1, df2]) / 10_000
 
     @property
-    def pose3d(self) -> np.ndarray[tuple[int, int], np.dtype[np.float32]]:
+    def positions(self) -> np.ndarray[tuple[int, int], np.dtype[np.float32]]:
+        """
+        Particle positions in micrograph fractions (X, Y).
+        """
+        xpos = self.get("location/center_x_frac", np.zeros(len(self), dtype=np.float32))
+        ypos = self.get("location/center_y_frac", np.zeros_like(xpos))
+        zpos = np.mean(self.defocus, axis=1, keepdims=True)
+        return np.column_stack([xpos, ypos, zpos])
+
+    @property
+    def rotations(self) -> np.ndarray[tuple[int, int], np.dtype[np.float32]]:
         """
         Pose 3D
         """
@@ -110,6 +118,38 @@ class CryoSPARC(Ensemble):
         if len(self.dset) == 0:
             raise ValueError("Dataset has no rows")
 
+    def _construct_fields(self) -> "list[dict[BobField, Any]]":
+        """
+        Construct named attributes from CryoSPARC fields.
+
+        Return a sorted list of funciton argument dicts.
+        We sort them here so they are presented in the same order they would be in
+        a CryoSPARC dataset.
+        """
+        fields = []
+        if not np.allclose((defocus := self.dset.defocus), 0.0):
+            fields.append(dict(data=defocus[:, 0], name="ctf/df1_um", atype="FLOAT"))
+            fields.append(dict(data=defocus[:, 1], name="ctf/df2_um", atype="FLOAT"))
+        if (shift3d := self.dset.shift3d) is not None:
+            fields.append(
+                dict(
+                    data=shift3d,
+                    name="alignments3D/shift",
+                    atype="FLOAT2",
+                )
+            )
+        if (shift2d := self.dset.shift2d) is not None:
+            fields.append(
+                dict(
+                    data=shift2d,
+                    name="alignments2D/shift",
+                    atype="FLOAT2",
+                )
+            )
+
+        # type checking complains about no matching overload here, but it's wrong
+        return sorted(fields, key=lambda field: field.get("name"))  # type: ignore
+
     def create_object(
         self,
         name: str = "CryoSPARC Ensemble",
@@ -118,22 +158,15 @@ class CryoSPARC(Ensemble):
         fraction: float = 1.0,
         simplify=False,
     ) -> db.BlenderObject:
-        defocus = self.dset.defocus.reshape((len(self.dset), -1)) * world_scale
-        positions = np.append(self.dset.positions, defocus, axis=1)
-        bob = db.create_bob(positions, name=name)
+        # build positions and rotation outside _construct_fields because we want
+        # them to appear first, like they would with a normal Blender object.
+        bob = db.create_bob(self.dset.positions, name=name)
         bob.store_named_attribute(
-            data=self.dset.pose3d, name="rotation", atype="QUATERNION"
+            data=self.dset.rotations, name="rotation", atype="QUATERNION"
         )
-        if (shift3d := self.dset.shift3d) is not None:
-            bob.store_named_attribute(
-                data=shift3d * world_scale,
-                name="alignments3D/shift",
-                atype="FLOAT2",
-            )
-        if (shift2d := self.dset.shift2d) is not None:
-            bob.store_named_attribute(
-                data=shift2d * world_scale,
-                name="alignments2D/shift",
-                atype="FLOAT2",
-            )
+        for field in self._construct_fields():
+            bob.store_named_attribute(**field)
+        if node_setup:
+            # TODO: make nodes etc.
+            print(simplify, fraction)
         return bob
