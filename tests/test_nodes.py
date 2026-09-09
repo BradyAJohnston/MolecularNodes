e60d51bae3601a71822a6da25f631d6c2de30ebf
Brady Johnston <brady.johnston@me.com>
2026-09-09 19:58:13 +0100

    cleanup unused node related code

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -4,15 +4,21 @@ import bpy
 import numpy as np
 import pytest
 from MDAnalysis.tests.datafiles import DCD, GRO, PSF, XTC
-from nodebpy.nodes.geometry import Group
+from nodebpy.nodes.geometry import Group, RealizeInstances, SetPosition
 import molecularnodes as mn
 from molecularnodes.nodes import nodes
 from molecularnodes.nodes.geometry import (
     BreakBonds,
     FindBonds,
+    NucleicChi,
+    NucleicDihedral,
+    PeptideChi,
+    PeptideDihedral,
+    PeriodicArray,
     SetColor,
     StyleCartoon,
 )
+from molecularnodes.nodes.nodes import custom_boolean_iswitch
 from .constants import codes, data_dir
 from .utils import GeometrySet, NumpySnapshotExtension
 
@@ -47,18 +53,18 @@ def test_selection():
 @pytest.mark.parametrize("code", codes)
 @pytest.mark.parametrize("attribute", ["chain_id", "entity_id"])
 def test_selection_working(snapshot_custom: NumpySnapshotExtension, attribute, code):
-    mol = mn.Molecule.fetch(code, cache=data_dir).add_style("ribbon")
-    group = mol.node_group
-    node_sel = nodes.add_selection(
-        group, mol.name, getattr(mol.props, f"{attribute}s"), attribute
-    )
+    mol = mn.Molecule.fetch(code, cache=data_dir)
+    with mol.tree.reset() as (atoms, join):
+        sel = Group()
+        sel.node.node_tree = custom_boolean_iswitch(
+            mol.name, getattr(mol.props, f"{attribute}s"), attribute
+        ).tree
+        atoms >> StyleCartoon(selection=sel) >> RealizeInstances() >> join
 
-    _n = len(node_sel.inputs)
+    _n = len(sel.i)
 
-    nodes.realize_instances(mol.object)
-
-    for inp in node_sel.inputs:
-        inp.default_value = True
+    for inp in sel.i:
+        inp.default_value = True  # type: ignore
         pos = mol.named_attribute("position", evaluate=True)
         assert snapshot_custom == pos.shape
         assert snapshot_custom == pos
@@ -77,7 +83,7 @@ def test_color_custom(snapshot_custom: NumpySnapshotExtension, code, attribute):
     )
     with mol.tree.reset() as (atoms, join):
         n_color = Group()
-        n_color.node.node_tree = group_col
+        n_color.node.node_tree = group_col.tree
 
         atoms >> SetColor(color=n_color) >> StyleCartoon() >> join
 
@@ -89,7 +95,7 @@ def test_color_custom(snapshot_custom: NumpySnapshotExtension, code, attribute):
 
 def test_iswitch_creation():
     items = [str(x) for x in range(10)]
-    tree_boolean = nodes.custom_boolean_iswitch("newboolean", items)
+    tree_boolean = nodes.custom_boolean_iswitch("newboolean", items).tree
     # ensure there isn't an item called 'Color' in the created interface
     assert not tree_boolean.interface.items_tree.get("Color")
     assert tree_boolean.interface.items_tree["Selection"].in_out == "OUTPUT"
@@ -97,7 +103,7 @@ def test_iswitch_creation():
     for i in items:
         assert tree_boolean.interface.items_tree[str(i)].in_out == "INPUT"
 
-    tree_rgba = nodes.custom_color_iswitch("newcolor", items)
+    tree_rgba = nodes.custom_color_iswitch("newcolor", items).tree
     # ensure there isn't an item called 'selection'
     assert not tree_rgba.interface.items_tree.get("Selection")
     assert tree_rgba.interface.items_tree["Color"].in_out == "OUTPUT"
@@ -110,7 +116,7 @@ def test_op_custom_color():
     mol.object.select_set(True)
     group = nodes.custom_color_iswitch(
         name=f"Color Chain {mol.name}", items=mol.props.chain_ids
-    )
+    ).tree
 
     assert group
     assert group.interface.items_tree["G"].name == "G"
@@ -125,104 +131,27 @@ def test_color_lookup_supplied():
         name=name,
         items={str(x): col for x in range(10, 20)},
         offset=10,
-    )
+    ).tree
     assert node.name == name
     for item in nodes.inputs(node).values():
         assert np.allclose(np.array(item.default_value), col)
 
-    node = nodes.custom_color_iswitch(name="test2", items=range(10, 20), offset=10)
+    node = nodes.custom_color_iswitch(name="test2", items=range(10, 20), offset=10).tree
     for item in nodes.inputs(node).values():
         assert not np.allclose(np.array(item.default_value), col)
 
 
-def get_links(sockets):
-    for socket in sockets:
-        for link in socket.links:
-            yield link
-
-
-@pytest.fixture
-def pdb_8h1b():
-    return mn.Molecule.fetch("8H1B", cache=data_dir)
-
-
-# topology_node_names = [n for n in dir(mn.nodes.geometry) if not n.startswith(".")]
-topology_node_names = []
-
-
-@pytest.mark.parametrize("node_name", topology_node_names)
-@pytest.mark.parametrize("code", codes)
-def test_node_topology(snapshot_custom: NumpySnapshotExtension, code, node_name):
-    mol = mn.Molecule.fetch(code, cache=data_dir)
-
-    group = nodes.get_mod(mol.object).node_group = nodes.new_tree()
-
-    group.links.new(
-        group.nodes["Group Input"].outputs[0], group.nodes["Group Output"].inputs[0]
-    )
-    node_att = group.nodes.new("GeometryNodeStoreNamedAttribute")
-    node_att.inputs[2].default_value = "test_attribute"
-    nodes.insert_last_node(group, node_att)
-    # exclude these particular nodes, as they aren't field nodes and so we shouldn't
-    # be testing them here. Will create their own particular tests later
-    if any(
-        keyword in node_name
-        for keyword in [
-            "Bonds",
-            "Bond Count",
-            "DSSP",
-            "Sample Atomic Attributes",
-            "Chain Group ID",
-            "Peptide Dihedral",
-            "Peptide Chi",
-            "Nucleic Dihedral",
-            "Nucleic Chi",
-        ]
-    ):
-        return None
-
-    node_topo = nodes.add_custom(
-        group, node_name, location=[x - 300 for x in node_att.location]
-    )
-
-    if mn.nodes.arrange.node_has_geo_socket(node_topo):
-        return None
-
-    if node_name == "Residue Mask":
-        node_topo.inputs["atom_name"].default_value = 61
-
-    type_to_data_type = {
-        "VECTOR": "FLOAT_VECTOR",
-        "VALUE": "FLOAT",
-        "BOOLEAN": "BOOLEAN",
-        "INT": "INT",
-        "RGBA": "FLOAT_COLOR",
-        "ROTATION": "QUATERNION",
-    }
-
-    for output in node_topo.outputs:
-        node_att.data_type = type_to_data_type[output.type]
-        input = node_att.inputs["Value"]
-
-        for link in input.links:
-            group.links.remove(link)
-
-        group.links.new(output, input)
-
-        assert snapshot_custom == mol.named_attribute("test_attribute", evaluate=True)
-
-
 @pytest.mark.parametrize(
-    "name", ["Peptide Dihedral", "Nucleic Dihedral", "Peptide Chi", "Nucleic Chi"]
+    "name", [PeptideDihedral, NucleicDihedral, PeptideChi, NucleicChi]
 )
 @pytest.mark.parametrize("code", ["8H1B", "1BNA"])
-def test_dihedral_rotations(snapshot_custom: NumpySnapshotExtension, code, name):
+def test_dihedral_rotations(snapshot_custom: NumpySnapshotExtension, code, node):
     mol = mn.Molecule.fetch(code, cache=data_dir)
-    node_sp = mn.nodes.nodes.insert_before(
-        mol.modifier_node_tree.nodes["Group Output"], "GeometryNodeSetPosition"
-    )
-    node = mn.nodes.nodes.insert_before(node_sp.inputs["Position"], name)
-    for input in node.inputs:
+    with mol.tree.reset() as (atoms, join):
+        pos = node()
+        (atoms >> SetPosition(position=pos) >> join)
+
+    for input in pos.i:
         if input.name in ["Position", "Selection"]:
             continue
         input.default_value = 1.0
@@ -276,12 +205,6 @@ def test_reuse_node_group():
     assert n_nodes == len(tree.nodes)
 
 
-def _insert_periodic_array(traj: mn.Molecule):
-    node = mn.nodes.nodes.add_custom(traj.modifier_node_tree, "Periodic Array")
-    mn.nodes.nodes.insert_last_node(group=traj.tree.tree, node=node)
-    return node
-
-
 def _get_node_defaults(node) -> list[Any]:
     defaults = []
     for input in node.inputs:
@@ -297,12 +220,15 @@ def _get_node_defaults(node) -> list[Any]:
 
 def test_periodic_array(snapshot, tmp_path):
     traj = mn.Molecule.load(GRO, XTC)
-    node = _insert_periodic_array(traj)
+
+    with traj.tree.reset() as (atoms, join):
+        node = PeriodicArray()
+        atoms >> node >> join
 
     traj.set_frame(1)
-    defaults_0 = _get_node_defaults(node)
+    defaults_0 = _get_node_defaults(node.node)
     traj.set_frame(10)
-    defaults_10 = _get_node_defaults(node)
+    defaults_10 = _get_node_defaults(node.node)
 
     dim_idx = slice(1, 7)
     assert not all([x == y for x, y in zip(defaults_0[dim_idx], defaults_10[dim_idx])])
@@ -321,12 +247,14 @@ def test_periodic_array(snapshot, tmp_path):
 # and everything remains 0
 def test_periodic_array_no_dimensions():
     traj = mn.Molecule.load(PSF, DCD)
-    node = _insert_periodic_array(traj)
+    with traj.tree.reset() as (atoms, join):
+        node = PeriodicArray()
+        atoms >> node >> join
 
     traj.set_frame(1)
-    defaults_0 = _get_node_defaults(node)
+    defaults_0 = _get_node_defaults(node.node)
     traj.set_frame(frame=10)
-    defaults_10 = _get_node_defaults(node)
+    defaults_10 = _get_node_defaults(node.node)
 
     assert defaults_0 == defaults_10
     assert defaults_0[1:7] == [0] * 6
