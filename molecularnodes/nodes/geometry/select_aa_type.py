7cc211722d61514f1eaa4f3306a5f81b707021db
Brady Johnston <brady.johnston@me.com>
2026-09-14 10:23:31 +0100

    rework group

--- molecularnodes/nodes/geometry/select_aa_type.py
+++ molecularnodes/nodes/geometry/select_aa_type.py
@@ -1,7 +1,9 @@
-# Node-group asset 'Select AA Type' (GeometryNodeTree), dumped by nodebpy.assets.dump_library.
+# Node-group asset "Select AA Type" (GeometryNodeTree), dumped by nodebpy.assets.dump_library.
 # Rebuild the library with nodebpy.assets.build_library (python -m nodebpy.assets build).
 # _build_group() is the source of truth: the docstring, __init__ and accessors are regenerated from it on the next dump.
 from typing import TYPE_CHECKING
+from bpy.types import GeometryNodeTree
+from nodebpy import TreeBuilder
 from nodebpy import geometry as g
 from nodebpy.builder import (
     AssetGeometryGroup,
@@ -14,7 +16,7 @@ from .boolean_andor import BooleanAndOr
 from .residue_name import ResidueName
 
 
-class SelectAaType(AssetGeometryGroup):
+class SelectAAType(AssetGeometryGroup):
     """
     Select AA Type
 
@@ -35,20 +37,20 @@ class SelectAaType(AssetGeometryGroup):
     Outputs
     -------
     o.is_polar : BooleanSocket
-        is_polar
+        The amino acid residue is polar
     o.is_apolar : BooleanSocket
-        is_apolar
+        The amino acid residue is apolar
     o.is_acidic : BooleanSocket
-        is_acidic
+        The amino acid residue is acidic
     o.is_basic : BooleanSocket
-        is_basic
+        The amino acid residue is basic
     o.is_aromatic : BooleanSocket
-        is_aromatic
+        The amino acid residue is aromatic
     """
 
     _name = "Select AA Type"
     _asset_name = "Select AA Type"
-    _library = PackageLibrary(__file__, "../../assets/node_data_file.blend")
+    _library = PackageLibrary(__file__, "../../assets/nodes.blend")
     _color_tag = "INPUT"
     _tree_properties = {"node_tool_idname": "geometry.select_aa_type"}
 
@@ -60,10 +62,15 @@ class SelectAaType(AssetGeometryGroup):
 
     class _Outputs(SocketAccessor):
         is_polar: BooleanSocket
+        """The amino acid residue is polar"""
         is_apolar: BooleanSocket
+        """The amino acid residue is apolar"""
         is_acidic: BooleanSocket
+        """The amino acid residue is acidic"""
         is_basic: BooleanSocket
+        """The amino acid residue is basic"""
         is_aromatic: BooleanSocket
+        """The amino acid residue is aromatic"""
 
     if TYPE_CHECKING:
 
@@ -79,7 +86,7 @@ class SelectAaType(AssetGeometryGroup):
     ):
         super().__init__(**{"And": and_, "Or": or_})
 
-    def _build_group(self, tree):
+    def _build_group(self, tree: TreeBuilder[GeometryNodeTree]) -> None:
         and_ = tree.inputs.boolean(
             "And",
             True,
@@ -92,162 +99,101 @@ class SelectAaType(AssetGeometryGroup):
             description="The resulting selection can be calculated from this node or be from this input selection",
             hide_value=True,
         )
-        is_polar = tree.outputs.boolean("is_polar")
-        is_apolar = tree.outputs.boolean("is_apolar")
-        is_acidic = tree.outputs.boolean("is_acidic")
-        is_basic = tree.outputs.boolean("is_basic")
-        is_aromatic = tree.outputs.boolean("is_aromatic")
-
-        boolean = g.Boolean(boolean=True)
-        group = ResidueName()
-
-        # Polar: ASN(2), CYS(4), GLN(6), HIS(8), SER(15), THR(16)
-        index_switch_polar = g.IndexSwitch.boolean(
-            group,
-            (
-                False,
-                False,
-                boolean,
-                False,
-                boolean,
-                False,
-                boolean,
-                False,
-                boolean,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                boolean,
-                boolean,
-                False,
-                False,
-                False,
-            )
-            + (False,) * 24,  # pad to 44 for DNA/RNA
+        is_polar = tree.outputs.boolean(
+            "is_polar", description="The amino acid residue is polar"
         )
-        BooleanAndOr(and_=and_, or_=or_, boolean=index_switch_polar) >> is_polar
-
-        # Apolar: ALA(0), GLY(7), ILE(9), LEU(10), MET(12), PRO(14), VAL(19)
-        index_switch_apolar = g.IndexSwitch.boolean(
-            group,
-            (
-                boolean,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                boolean,
-                False,
-                boolean,
-                boolean,
-                False,
-                boolean,
-                False,
-                boolean,
-                False,
-                False,
-                False,
-                False,
-                boolean,
-            )
-            + (False,) * 24,
+        is_apolar = tree.outputs.boolean(
+            "is_apolar", description="The amino acid residue is apolar"
         )
-        BooleanAndOr(and_=and_, or_=or_, boolean=index_switch_apolar) >> is_apolar
-
-        # Acidic: ASP(3), GLU(5)
-        index_switch_acidic = g.IndexSwitch.boolean(
-            group,
-            (
-                False,
-                False,
-                False,
-                boolean,
-                False,
-                boolean,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-            )
-            + (False,) * 24,
+        is_acidic = tree.outputs.boolean(
+            "is_acidic", description="The amino acid residue is acidic"
         )
-        BooleanAndOr(and_=and_, or_=or_, boolean=index_switch_acidic) >> is_acidic
-
-        # Basic: ARG(1), LYS(11)
-        index_switch_basic = g.IndexSwitch.boolean(
-            group,
-            (
-                False,
-                boolean,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                boolean,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-            )
-            + (False,) * 24,
+        is_basic = tree.outputs.boolean(
+            "is_basic", description="The amino acid residue is basic"
         )
-        BooleanAndOr(and_=and_, or_=or_, boolean=index_switch_basic) >> is_basic
-
-        # Aromatic: PHE(13), TRP(17), TYR(18)
-        index_switch_aromatic = g.IndexSwitch.boolean(
-            group,
-            (
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                False,
-                boolean,
-                False,
-                False,
-                False,
-                boolean,
-                boolean,
-                False,
-            )
-            + (False,) * 24,
+        is_aromatic = tree.outputs.boolean(
+            "is_aromatic", description="The amino acid residue is aromatic"
         )
-        BooleanAndOr(and_=and_, or_=or_, boolean=index_switch_aromatic) >> is_aromatic
+
+        residue_name = ResidueName()
+        with g.Frame("Residue Category"):
+            index_switch = g.IndexSwitch.integer(
+                residue_name,
+                (
+                    1,
+                    2,
+                    0,
+                    3,
+                    0,
+                    3,
+                    0,
+                    1,
+                    0,
+                    1,
+                    1,
+                    2,
+                    1,
+                    4,
+                    1,
+                    0,
+                    0,
+                    4,
+                    4,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                ),
+            )
+            _string = g.String(
+                string="# ALA(0)\n# ARG(1)\n# ASN(2)\n# ASP(3)\n# CYS(4)\n# GLU(5)\n# GLN(6)\n# GLY(7)\n# HIS(8)\n# ILE(9)\n# LEU(10)\n# LYS(11)\n# MET(12)\n# PHE(13)\n# PRO(14)\n# SER(15)\n# THR(16)\n# TRP(17)\n# TYR(18)\n# VAL(19)"
+            )
+        switch = g.Compare.integer.equal(residue_name, -1).o.result.switch.integer(
+            index_switch, -1
+        )
+        (
+            BooleanAndOr(and_=and_, or_=or_, boolean=g.Compare.integer.equal(switch, 0))
+            >> is_polar
+        )
+        (
+            BooleanAndOr(and_=and_, or_=or_, boolean=g.Compare.integer.equal(switch, 1))
+            >> is_apolar
+        )
+        (
+            BooleanAndOr(and_=and_, or_=or_, boolean=g.Compare.integer.equal(switch, 3))
+            >> is_acidic
+        )
+        (
+            BooleanAndOr(and_=and_, or_=or_, boolean=g.Compare.integer.equal(switch, 2))
+            >> is_basic
+        )
+        (
+            BooleanAndOr(and_=and_, or_=or_, boolean=g.Compare.integer.equal(switch, 4))
+            >> is_aromatic
+        )
 
 
-ASSET = SelectAaType
+ASSET = SelectAAType
 
 ASSET_METADATA = {
     "catalog_id": "bd1f205b-fea5-4700-b2c2-754f3321e969",
