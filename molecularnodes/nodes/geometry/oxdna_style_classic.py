710401c58ccd29bf61f4774a10abf10201666e71
Daniel <danielhollis100@gmail.com>
2026-09-17 01:12:07 -0400

    Change default `End Overhang` input to better complement current default geometry

--- molecularnodes/nodes/geometry/oxdna_style_classic.py
+++ molecularnodes/nodes/geometry/oxdna_style_classic.py
@@ -281,7 +281,7 @@ class OxDNAStyleClassic(AssetGeometryGroup):
         backbone_radius: InputFloat = 1.0,
         ball_radius: InputFloat = 2.0,
         _5_3_taper: InputFloat = 0.0,
-        end_overhang: InputFloat = 1.0,
+        end_overhang: InputFloat = 1.2,
         base_shape: InputMenu | Literal["Sphere", "Cylinder", "None"] = "Sphere",
         base_geometry: InputGeometry = None,
         base_scale: InputVector = None,
@@ -357,7 +357,7 @@ class OxDNAStyleClassic(AssetGeometryGroup):
             n_5_3_taper = tree.inputs.float(
                 "5'→3' Taper", 0.0, min_value=0.0, max_value=1.0, subtype="FACTOR"
             )
-            end_overhang = tree.inputs.float("End Overhang", 1.0)
+            end_overhang = tree.inputs.float("End Overhang", 1.2)
         with tree.inputs.panel("Bases"):
             base_shape = tree.inputs.menu(
                 "Base Shape", expanded=True, optional_label=True
