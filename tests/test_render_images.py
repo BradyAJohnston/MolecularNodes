9de22d44e7c582c97344aba545f2827371b00d20
Daniel <danielhollis100@gmail.com>
2026-09-17 01:45:19 -0400

    Update `test_render_images` with new style node terminology

--- tests/test_render_images.py
+++ tests/test_render_images.py
@@ -166,7 +166,7 @@ def test_render_assembly(
                 ["linear.top", "linear_traj.dat"],
                 ["origami_old.top", "origami_old.dat"],
             ),
-            ("Arrows", "Curve"),
+            ("ball_and_stick", "ribbon"),
         )
     ),
 )
