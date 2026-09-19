56aa9d5f03c7b05465402bf27c99a6cc6fd71026
Brady Johnston <36021261+BradyAJohnston@users.noreply.github.com>
2026-09-19 10:07:05 +0100

    Fix #1229: Building elastic networks (#1233)
    The `Plexus` input node was missing a scale input. Have return `VDWRadii` to the input which fixes elastic network building.
    
    Simulation of the elastic network still isn't working great but this fixes part of #1229 but not all of it.

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -23,6 +23,7 @@ from molecularnodes.nodes._utils import (
 )
 from molecularnodes.nodes.geometry import (
     BreakBonds,
+    BuildElasticNetwork,
     Charge,
     FindBonds,
     NucleicChi,
@@ -327,6 +328,18 @@ def test_segment_id(topology, trajectory, n_segments):
         )
 
     assert np.array_equal(traj.named_attribute("node_segid", evaluate=True), expected)
+
+
+def test_build_elastic_network():
+    mol = mn.Molecule.fetch("4ozs")
+
+    with mol.tree.reset() as (atoms, join):
+        BuildElasticNetwork(atoms) >> join
+
+    gs = GeometrySet(mol.object)
+    assert gs.mesh
+    assert len(gs.mesh.edges) == 1049
+    assert len(gs.mesh.vertices) == sum(mol["is_alpha_carbon"])
 
 
 def test_evaluate_on_atoms_bundle():
